30ddc125fcddc7472b97b5663db1af471d408cde
ygo <ka.ra.si.naporitan@gmail.com>
2024-02-01 10:43:59 +0900

    discordgoインストール

--- core/main.go
+++ core/main.go
@@ -1,1 +1,22 @@
 package core
+
+import (
+	"github.com/bwmarrin/discordgo"
+)
+
+func main() {
+	Token := "Bot " //"Bot"という接頭辞がないと401 unauthorizedエラーが起きます
+	discord, err := discordgo.New(Token)
+
+	// 権限追加
+	discord.Identify.Intents = discordgo.MakeIntent(discordgo.IntentsAll)
+	discord.Token = Token
+	if err != nil {
+		panic(err)
+	}
+	// websocketを開いてlistening開始
+	if err = discord.Open(); err != nil {
+		panic("Error while opening session")
+	}
+
+}

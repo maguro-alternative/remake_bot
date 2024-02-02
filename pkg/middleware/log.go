85ac990b2d98e53dde8cb1d616cb74ea6411bd49
ygo <ka.ra.si.naporitan@gmail.com>
2024-02-02 12:53:03 +0900

    logのミドルウェア追加

--- pkg/middleware/log.go
+++ pkg/middleware/log.go
@@ -1,1 +1,13 @@
 package middleware
+
+import (
+	"log"
+	"net/http"
+)
+
+func LogMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		next.ServeHTTP(w, r)
+		log.Println(r.Method, r.URL.Path, r.RemoteAddr, r.UserAgent())
+	})
+}

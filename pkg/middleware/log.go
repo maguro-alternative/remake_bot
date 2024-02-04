cdcf991c7d4f6a8b7cf6664bd0d615807d3234c6
ygo <ka.ra.si.naporitan@gmail.com>
2024-02-04 10:08:45 +0900

    コメント追記

--- pkg/middleware/log.go
+++ pkg/middleware/log.go
@@ -7,6 +7,7 @@ import (
 
 func LogMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// next.ServeHTTP(w, r)を実行する前にログを出力
 		next.ServeHTTP(w, r)
 		log.Println(r.Method, r.URL.Path, r.RemoteAddr, r.UserAgent())
 	})

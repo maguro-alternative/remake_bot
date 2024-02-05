72575a7ad394917059a562bfa998c8067f991798
ygo <ka.ra.si.naporitan@gmail.com>
2024-02-05 16:33:33 +0900

    bot部分の基礎

--- bot/main.go
+++ bot/main.go
@@ -1,1 +1,46 @@
 package bot
+
+import (
+	"github.com/maguro-alternative/remake_bot/bot/config"
+	"github.com/maguro-alternative/remake_bot/pkg/db"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/cockroachdb/errors"
+)
+
+/*
+スラッシュコマンドとハンドラの登録
+
+スラッシュコマンドとハンドラの登録は、
+discordgo.Session.ApplicationCommandCreate()と
+discordgo.Session.AddHandler()を使って行います。
+*/
+
+type Command struct {
+	Name        string
+	Aliases     []string
+	Description string
+	Options     []*discordgo.ApplicationCommandOption
+	AppCommand  *discordgo.ApplicationCommand
+	Executor    func(s *discordgo.Session, i *discordgo.InteractionCreate)
+}
+
+type Handler struct {
+	session  *discordgo.Session
+	commands map[string]*Command
+	guild    string
+}
+
+func BotOnReady(indexDB db.Driver) (*discordgo.Session, error) {
+	discordSession, err := discordgo.New("Bot " + config.Token())
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	discordSession.Identify.Intents = discordgo.IntentsAll
+	discordSession.Token = config.Token()
+	err = discordSession.Open()
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	return discordSession, nil
+}

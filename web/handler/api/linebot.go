35961e9b567bbfc3a4e0d897bea0cf55ddab3d62
ygo <ka.ra.si.naporitan@gmail.com>
2024-02-07 11:26:36 +0900

    aes検証仮実装

--- web/handler/api/linebot.go
+++ web/handler/api/linebot.go
@@ -3,11 +3,15 @@ package api
 import (
 	"net/http"
 	"context"
-	//"io"
-	//"encoding/hex"
+	"io"
+	"log"
+	"crypto/hmac"
+    "crypto/sha256"
+	"encoding/hex"
 
 	"github.com/maguro-alternative/remake_bot/web/service"
-	//"github.com/maguro-alternative/remake_bot/pkg/crypto"
+	"github.com/maguro-alternative/remake_bot/web/config"
+	"github.com/maguro-alternative/remake_bot/pkg/crypto"
 )
 
 // A LineBotHandler handles requests for the line bot.
@@ -22,14 +26,69 @@ func NewLineBotHandler(indexService *service.IndexService) *LineBotHandler {
 	}
 }
 
+type LineSecret struct {
+	ClientSercret []byte `db:"line_client_sercret"`
+}
+
 // ServeHTTP handles HTTP requests.
 func (h *LineBotHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
+	var iv []byte
+	privateKey := config.PrivateKey()
 	ctx := r.Context()
 	if ctx == nil {
 		ctx = context.Background()
+	}
+
+	requestBodyByte, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Println("Failed to Load Request")
+		//response.BadRequest(writer, "Failed to Load Request")
+		return
+	}
+
+	keyBytes, err := hex.DecodeString(privateKey)
+	if err != nil {
+		log.Println("Failed to Load Request")
+		return
+	}
+
+	var lineClientSecrets []LineSecret
+	err = h.IndexService.DB.SelectContext(ctx, &lineClientSecrets, "TestSercretKey")
+	if err != nil {
+		log.Println("Failed to Load Request")
+		return
+	}
+
+	for _, lineClientSecret := range lineClientSecrets {
+		err = h.IndexService.DB.GetContext(ctx, &iv, "TestSercret")
+		if err != nil {
+			log.Println("Failed to Load Request")
+			return
+		}
+
+		sercretKey, err := crypto.Decrypt(lineClientSecret.ClientSercret, keyBytes, iv)
+		if err != nil {
+			log.Println("Failed to Load Request")
+			return
+		}
+		inputSign := r.Header.Get("X-Line-Signature")
+		// 受け取った署名をStringからByteへ変換
+		inputSignByte, err := hex.DecodeString(inputSign)
+		if err != nil {
+			return
+		}
+
+		// macの生成
+		mac := hmac.New(sha256.New, []byte(sercretKey))
+		mac.Write(requestBodyByte)
+		validSignByte := mac.Sum(nil)
+
+		if hmac.Equal(inputSignByte, validSignByte) {
+			break
+		}
 	}
 }

c27e035f64d56f244a3929882d4176e233c5b67e
ygo <ka.ra.si.naporitan@gmail.com>
2024-02-10 15:15:07 +0900

    プロフィールの型追加

--- web/handler/api/linebot/internal/entity.go
+++ web/handler/api/linebot/internal/entity.go
@@ -1,5 +1,9 @@
 package internal
 
+import (
+	validation "github.com/go-ozzo/ozzo-validation"
+)
+
 type LineBot struct {
 	LineNotifyToken   []byte `db:"line_notify_token"`
 	LineBotToken      []byte `db:"line_bot_token"`
@@ -20,4 +24,21 @@ type LineBotDecrypt struct {
 	LineClientSercret string
 	DefaultChannelID  string
 	DebugMode         bool
+}
+
+type LineProfile struct {
+	DisplayName   string `json:"displayName"`
+	UserID        string `json:"userId"`
+	PictureUrl    string `json:"pictureUrl"`
+	StatusMessage string `json:"statusMessage"`
+	Message       string `json:"message"`
+}
+
+func (l *LineProfile) Validate() error {
+	return validation.ValidateStruct(l,
+		validation.Field(&l.DisplayName, validation.Required),
+		validation.Field(&l.UserID, validation.Required),
+		validation.Field(&l.PictureUrl, validation.Required),
+		validation.Field(&l.StatusMessage, validation.Required),
+	)
 }

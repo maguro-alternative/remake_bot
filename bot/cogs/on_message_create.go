442d0db82aa0349119c061621023c6ae4dbd1918
ygo <ka.ra.si.naporitan@gmail.com>
2024-02-14 08:22:42 +0900

    音声処理

--- bot/cogs/on_message_create.go
+++ bot/cogs/on_message_create.go
@@ -3,7 +3,13 @@ package cogs
 import (
 	"context"
 	"encoding/hex"
+	"os/exec"
+	"os"
+	"io"
 	"path/filepath"
+	"net/http"
+	"regexp"
+	"strconv"
 
 	"github.com/maguro-alternative/remake_bot/pkg/crypto"
 	"github.com/maguro-alternative/remake_bot/pkg/line"
@@ -18,7 +24,6 @@ type Repository interface {
 	GetLineChannel(ctx context.Context, channelID string) (onMessageCreate.LineChannel, error)
 	InsertLineChannel(ctx context.Context, channelID string, guildID string) error
 }
-
 
 func (h *CogHandler) OnMessageCreate(s *discordgo.Session, vs *discordgo.MessageCreate) {
 	var channel onMessageCreate.LineChannel
@@ -78,22 +83,23 @@ func (h *CogHandler) OnMessageCreate(s *discordgo.Session, vs *discordgo.Message
 
 	switch vs.ReferencedMessage.Type {
 	case discordgo.MessageTypeUserPremiumGuildSubscription:
-		sendText = vs.Message.Author.Username+"がサーバーブーストしました。"
+		sendText = vs.Message.Author.Username + "がサーバーブーストしました。"
 	case discordgo.MessageTypeUserPremiumGuildSubscriptionTierOne:
-		sendText = vs.Message.Author.Username+"がサーバーブーストし、レベル1になりました！！！！！！！！"
+		sendText = vs.Message.Author.Username + "がサーバーブーストし、レベル1になりました！！！！！！！！"
 	case discordgo.MessageTypeUserPremiumGuildSubscriptionTierTwo:
-		sendText = vs.Message.Author.Username+"がサーバーブーストし、レベル2になりました！！！！！！！！"
+		sendText = vs.Message.Author.Username + "がサーバーブーストし、レベル2になりました！！！！！！！！"
 	case discordgo.MessageTypeUserPremiumGuildSubscriptionTierThree:
-		sendText = vs.Message.Author.Username+"がサーバーブーストし、レベル3になりました！！！！！！！！"
+		sendText = vs.Message.Author.Username + "がサーバーブーストし、レベル3になりました！！！！！！！！"
 	case discordgo.MessageTypeGuildMemberJoin:
-		sendText = vs.Message.Author.Username+"が参加しました。"
+		sendText = vs.Message.Author.Username + "が参加しました。"
 	default:
-		sendText = vs.Message.Author.Username+"「 "+vs.Message.Content+" 」"
+		sendText = vs.Message.Author.Username + "「 " + vs.Message.Content + " 」"
 	}
 
 	for _, attachment := range vs.Message.Attachments {
 		extension := filepath.Ext(attachment.Filename)
-		switch extension{
+		fileNameNoExt := filepath.Base(attachment.Filename[:len(attachment.Filename)-len(extension)])
+		switch extension {
 		case ".png":
 		case ".jpg":
 		case ".jpeg":
@@ -113,8 +119,67 @@ func (h *CogHandler) OnMessageCreate(s *discordgo.Session, vs *discordgo.Message
 		case ".mp3":
 		case ".wav":
 		case ".ogg":
+			tmpFile := os.TempDir()+"/"+attachment.Filename
+			tmpFileNotExt := os.TempDir()+"/"+fileNameNoExt
+			downloadFilePath, err := downloadFile(tmpFile, attachment.URL)
+			if err != nil {
+				return
+			}
+			err = exec.CommandContext(ctx, "ffmpeg", "-i", downloadFilePath, tmpFileNotExt+".m4a").Run()
+			if err != nil {
+				return
+			}
+			f, err := os.Open(tmpFileNotExt+".m4a")
+			if err != nil {
+				return
+			}
+			defer f.Close()
+			messsage, err := s.ChannelFileSend(
+				vs.ChannelID,
+				"m4aに変換します。"+tmpFileNotExt+".m4a",
+				f,
+			)
+			if err != nil {
+				return
+			}
+			cmd := exec.CommandContext(
+				ctx,
+				"ffprobe",
+				"-hide_banner",
+				tmpFileNotExt+".m4a",
+				"show_entries",
+				"format=duration",
+			)
+			out, err := cmd.CombinedOutput()
+			if err != nil {
+				return
+			}
+			match := regexp.MustCompile(`(\d+\.\d+)`).FindStringSubmatch(string(out))
+			audioLen, err := strconv.Atoi(match[0])
+			if err != nil {
+				return
+			}
+			audio := lineRequ.NewLineAudioMessage(
+				messsage.Attachments[0].URL,
+				audioLen,
+			)
+			lineMessageTypes = append(lineMessageTypes, &audio)
 		case ".m4a":
-
 		}
 	}
+}
+
+func downloadFile(tmpFilePath, url string) (string, error) {
+	f, err := os.Create(tmpFilePath)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	_, err = io.Copy(f, resp.Body)
+	return "", err
 }

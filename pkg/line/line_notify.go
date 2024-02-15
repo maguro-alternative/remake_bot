c5208ae26bd1b9bba96d6d658f01738f20a2c9ff
ygo <ka.ra.si.naporitan@gmail.com>
2024-02-15 09:57:51 +0900

    call追加

--- pkg/line/line_notify.go
+++ pkg/line/line_notify.go
@@ -50,12 +50,17 @@ func (l *LineNotifySticker) Validate() error {
 	)
 }
 
+type LineNotifyCall struct {
+	c *http.Client
+	r *http.Request
+}
+
 // LINE Notifyでメッセージを送信
 func (r *LineRequest) PushMessageNotify(ctx context.Context, message string) error {
 	client := &http.Client{}
 	url := "https://notify-api.line.me/api/notify"
 	notifyMessage := LineNotifyMessage{
-		Message: "message: " + message,
+		Message: message,
 	}
 	err := notifyMessage.Validate()
 	if err != nil {
@@ -85,7 +90,9 @@ func (r *LineRequest) PushImageNotify(ctx context.Context, message, image string
 	client := &http.Client{}
 	url := "https://notify-api.line.me/api/notify"
 	notifyMessage := LineNotifyImage{
-		Message: message,
+		Message:        message,
+		ImageThumbnail: image,
+		ImageFullsize:  image,
 	}
 	err := notifyMessage.Validate()
 	if err != nil {
@@ -140,4 +147,95 @@ func (r *LineRequest) PushStampNotify(ctx context.Context, message, stickerPacka
 	defer resp.Body.Close()
 
 	return nil
+}
+
+// LINE Notifyのメッセージを送信するためのリクエストを作成(実行はDo()で行う)
+func (r *LineRequest) PushMessageNotifyCall(ctx context.Context, message string) (*LineNotifyCall, error) {
+	client := &http.Client{}
+	url := "https://notify-api.line.me/api/notify"
+	notifyMessage := LineNotifyMessage{
+		Message: message,
+	}
+	err := notifyMessage.Validate()
+	if err != nil {
+		return nil, err
+	}
+	notifyMessageByte, err := json.Marshal(notifyMessage)
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequestWithContext(ctx, "POST", url, bytes.NewBuffer(notifyMessageByte))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Authorization", "Bearer "+r.lineNotifyToken)
+	return &LineNotifyCall{
+		c: client,
+		r: req,
+	}, nil
+}
+
+// LINE Notifyの画像を送信するためのリクエストを作成(実行はDo()で行う)
+func (r *LineRequest) PushImageNotifyCall(ctx context.Context, message, image string) (*LineNotifyCall, error) {
+	client := &http.Client{}
+	url := "https://notify-api.line.me/api/notify"
+	notifyMessage := LineNotifyImage{
+		Message: message,
+		ImageThumbnail: image,
+		ImageFullsize:  image,
+	}
+	err := notifyMessage.Validate()
+	if err != nil {
+		return nil, err
+	}
+	notifyMessageByte, err := json.Marshal(notifyMessage)
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequestWithContext(ctx, "POST", url, bytes.NewBuffer(notifyMessageByte))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Authorization", "Bearer "+r.lineNotifyToken)
+	return &LineNotifyCall{
+		c: client,
+		r: req,
+	}, nil
+}
+
+// LINE Notifyのスタンプを送信するためのリクエストを作成(実行はDo()で行う)
+func (r *LineRequest) PushStampNotifyCall(ctx context.Context, message, stickerPackageID, stickerID string) (*LineNotifyCall, error) {
+	client := &http.Client{}
+	url := "https://notify-api.line.me/api/notify"
+	notifyMessage := LineNotifySticker{
+		Message:          message,
+		StickerPackageID: stickerPackageID,
+		StickerID:        stickerID,
+	}
+	err := notifyMessage.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	notifyMessageByte, err := json.Marshal(notifyMessage)
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequestWithContext(ctx, "POST", url, bytes.NewBuffer(notifyMessageByte))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Authorization", "Bearer "+r.lineNotifyToken)
+	return &LineNotifyCall{
+		c: client,
+		r: req,
+	}, nil
+}
+
+// LINE Notifyのメッセージを送信するためのリクエストを実行
+func (l *LineNotifyCall) Do() (*http.Response, error) {
+	return l.c.Do(l.r)
 }

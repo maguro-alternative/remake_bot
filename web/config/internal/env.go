b3c3b7a436724d7b87c7abaf3da84208981163ef
ygo <ka.ra.si.naporitan@gmail.com>
2024-02-17 14:03:21 +0900

    環境変数名を変更

--- web/config/internal/env.go
+++ web/config/internal/env.go
@@ -1,12 +1,12 @@
 package internal
 
 type Config struct {
-	PrivateKey    string `env:"PRIVATE_KEY" envDefault:""`
-	YouTubeAPIKey string `env:"YOUTUBE_API_KEY" envDefault:""`
-	YoutubeAccessToken string `env:"YOUTUBE_ACCESS_TOKEN" envDefault:""`
-	YoutubeClientID string `env:"YOUTUBE_CLIENT_ID" envDefault:""`
+	PrivateKey          string `env:"PRIVATE_KEY" envDefault:""`
+	YouTubeAPIKey       string `env:"YOUTUBE_API_KEY" envDefault:""`
+	YoutubeAccessToken  string `env:"YOUTUBE_ACCESS_TOKEN" envDefault:""`
+	YoutubeClientID     string `env:"YOUTUBE_CLIENT_ID" envDefault:""`
 	YoutubeClientSecret string `env:"YOUTUBE_CLIENT_SECRET" envDefault:""`
-	YoutubeRefreshToken string `env:"Youtube_REFRESH_TOKEN" envDefault:""`
-	YoutubeProjectID string `env:"YOUTUBE_PROJECT_ID" envDefault:""`
-	YoutubeTokenExpiry string `env:"YOUTUBE_TOKEN_EXPIRY" envDefault:""`
+	YoutubeRefreshToken string `env:"YOUTUBE_REFRESH_TOKEN" envDefault:""`
+	YoutubeProjectID    string `env:"YOUTUBE_PROJECT_ID" envDefault:""`
+	YoutubeTokenExpiry  string `env:"YOUTUBE_TOKEN_EXPIRY" envDefault:""`
 }

d3b21ff4aa05983f24876740ed6ca1c224910ae7
ygo <ka.ra.si.naporitan@gmail.com>
2024-02-18 22:10:11 +0900

    updateの構文条件変更

--- web/handler/api/linetoken/internal/entity.go
+++ web/handler/api/linetoken/internal/entity.go
@@ -35,13 +35,18 @@ type LineBot struct {
 	LineBotToken     []byte `db:"line_bot_token"`
 	LineBotSecret    []byte `db:"line_bot_secret"`
 	LineGroupID      []byte `db:"line_group_id"`
+	LineClientID     []byte `db:"line_client_id"`
+	LineClientSecret []byte `db:"line_client_secret"`
 	DefaultChannelID string `db:"default_channel_id"`
 	DebugMode        bool   `db:"debug_mode"`
 }
 
 type LineBotIv struct {
-	LineNotifyTokenIv []byte `db:"line_notify_token_iv"`
-	LineBotTokenIv    []byte `db:"line_bot_token_iv"`
-	LineBotSecretIv   []byte `db:"line_bot_secret_iv"`
-	LineGroupIDIv     []byte `db:"line_group_id_iv"`
+	GuildID            string `db:"guild_id"`
+	LineNotifyTokenIv  []byte `db:"line_notify_token_iv"`
+	LineBotTokenIv     []byte `db:"line_bot_token_iv"`
+	LineBotSecretIv    []byte `db:"line_bot_secret_iv"`
+	LineGroupIDIv      []byte `db:"line_group_id_iv"`
+	LineClientIDIv     []byte `db:"line_client_id_iv"`
+	LineClientSecretIv []byte `db:"line_client_secret_iv"`
 }
--- web/handler/api/linetoken/internal/repository.go
+++ web/handler/api/linetoken/internal/repository.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"reflect"
+	"strings"
 
 	"github.com/maguro-alternative/remake_bot/pkg/db"
 )
@@ -18,9 +19,51 @@ func NewRepository(db db.Driver) *Repository {
 	}
 }
 
-func (r *Repository) UpdateLineBot(ctx context.Context, lineBotJson *LineBotJson) error {
+func (r *Repository) UpdateLineBot(ctx context.Context, lineBot *LineBot) error {
 	var setNameQuery string
-	structTypeOf := reflect.TypeOf(lineBotJson)
+	var setQueryArray []string
+
+	if len(lineBot.LineNotifyToken) > 0 {
+		setQueryArray = append(setQueryArray, "line_notify_token = :line_notify_token")
+	}
+	if len(lineBot.LineBotToken) > 0 {
+		setQueryArray = append(setQueryArray, "line_bot_token = :line_bot_token")
+	}
+	if len(lineBot.LineBotSecret) > 0 {
+		setQueryArray = append(setQueryArray, "line_bot_secret = :line_bot_secret")
+	}
+	if len(lineBot.LineGroupID) > 0 {
+		setQueryArray = append(setQueryArray, "line_group_id = :line_group_id")
+	}
+	if len(lineBot.LineClientID) > 0 {
+		setQueryArray = append(setQueryArray, "line_client_id = :line_client_id")
+	}
+	if len(lineBot.LineClientSecret) > 0 {
+		setQueryArray = append(setQueryArray, "line_client_secret = :line_client_secret")
+	}
+	if lineBot.DefaultChannelID != "" {
+		setQueryArray = append(setQueryArray, "default_channel_id = :default_channel_id")
+	}
+	if lineBot.DebugMode {
+		setQueryArray = append(setQueryArray, "debug_mode = :debug_mode")
+	}
+	setNameQuery = strings.Join(setQueryArray, ",")
+
+	query := fmt.Sprintf(`
+		UPDATE
+			line_bot
+		SET
+			%s
+		WHERE
+			guild_id = :guild_id
+	`, setNameQuery)
+	_, err := r.db.NamedExecContext(ctx, query, lineBot)
+	return err
+}
+
+func (r *Repository) UpdateLineBotIv(ctx context.Context, lineBotIv *LineBotIv) error {
+	var setNameQuery string
+	structTypeOf := reflect.TypeOf(lineBotIv)
 
 	// 受け取った構造体のフィールドのみを更新する
 	for i := 0; i < structTypeOf.NumField(); i++ {
@@ -36,12 +79,12 @@ func (r *Repository) UpdateLineBot(ctx context.Context, lineBotJson *LineBotJson
 	}
 	query := fmt.Sprintf(`
 		UPDATE
-			line_bot
+			line_bot_iv
 		SET
 			%s
 		WHERE
 			guild_id = :guild_id
 	`, setNameQuery)
-	_, err := r.db.NamedExecContext(ctx, query, lineBotJson)
+	_, err := r.db.NamedExecContext(ctx, query, lineBotIv)
 	return err
 }

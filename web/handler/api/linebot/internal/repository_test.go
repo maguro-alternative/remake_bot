808fdd23f9c11aa390f010a0a405ff56fabf2876
ygo <ka.ra.si.naporitan@gmail.com>
2024-02-19 08:11:32 +0900

    テスト

--- web/handler/api/linebot/internal/repository_test.go
+++ web/handler/api/linebot/internal/repository_test.go
@@ -33,4 +33,49 @@ func TestRepository_UpdateLineBot(t *testing.T) {
 			lb.DebugMode = false
 		}),
 	)
+
+	repo := NewRepository(tx)
+	t.Run("GuildIDからLineBotを取得できること", func(t *testing.T) {
+		lineBots, err := repo.GetLineBots(ctx)
+		assert.NoError(t, err)
+		assert.Equal(t, 1, len(lineBots))
+		assert.Equal(t, "987654321", lineBots[0].GuildID)
+		assert.Equal(t, []byte("123456789"), lineBots[0].LineNotifyToken)
+		assert.Equal(t, []byte("123456789"), lineBots[0].LineBotToken)
+		assert.Equal(t, []byte("123456789"), lineBots[0].LineBotSecret)
+		assert.Equal(t, []byte("987654321"), lineBots[0].LineGroupID)
+		assert.Equal(t, "987654321", lineBots[0].DefaultChannelID)
+		assert.Equal(t, false, lineBots[0].DebugMode)
+	})
+}
+
+func TestRepository_GetLineBotIv(t *testing.T) {
+	ctx := context.Background()
+	dbV1, cleanup, err := db.NewDB(ctx, config.DatabaseName(), config.DatabaseURL())
+	assert.NoError(t, err)
+	defer cleanup()
+	tx, err := dbV1.BeginTxx(ctx, nil)
+	assert.NoError(t, err)
+
+	defer tx.RollbackCtx(ctx)
+
+	f := &fixtures.Fixture{DBv1: tx}
+	f.Build(t,
+		fixtures.NewLineBotIv(ctx, func(lbi *fixtures.LineBotIv) {
+			lbi.GuildID = "987654321"
+			lbi.LineNotifyTokenIv = []byte("123456789")
+			lbi.LineBotTokenIv = []byte("123456789")
+			lbi.LineBotSecretIv = []byte("123456789")
+			lbi.LineGroupIDIv = []byte("987654321")
+		}),
+	)
+	repo := NewRepository(tx)
+	t.Run("GuildIDからLineBotIvを取得できること", func(t *testing.T) {
+		lineBotIv, err := repo.GetLineBotIv(ctx, "987654321")
+		assert.NoError(t, err)
+		assert.Equal(t, []byte("123456789"), lineBotIv.LineNotifyTokenIv)
+		assert.Equal(t, []byte("123456789"), lineBotIv.LineBotTokenIv)
+		assert.Equal(t, []byte("123456789"), lineBotIv.LineBotSecretIv)
+		assert.Equal(t, []byte("987654321"), lineBotIv.LineGroupIDIv)
+	})
 }

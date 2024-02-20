364c5e0b03ac9cf6b9b0b630c84f656c67af4a55
ygo <ka.ra.si.naporitan@gmail.com>
2024-02-21 08:46:46 +0900

    テスト追加

--- web/handler/api/linechannel/internal/repository.go
+++ web/handler/api/linechannel/internal/repository.go
@@ -81,7 +81,7 @@ func (r *Repository) DeleteNotInsertLineNgDiscordMessageTypes(ctx context.Contex
 
 func (r *Repository) InsertLineNgDiscordIDs(ctx context.Context, lineNgIDs []LineNgID) error {
 	query := `
-		INSERT INTO line_ng_id (
+		INSERT INTO line_ng_discord_id (
 			channel_id,
 			guild_id,
 			id,
@@ -110,7 +110,7 @@ func (r *Repository) DeleteNotInsertLineNgDiscordIDs(ctx context.Context, lineNg
 	// INSERT されるもの以外を削除
 	query := fmt.Sprintf(`
 	DELETE FROM
-		line_ng_id
+		line_ng_discord_id
 	WHERE NOT EXISTS (
 		SELECT
 			*
@@ -120,9 +120,9 @@ func (r *Repository) DeleteNotInsertLineNgDiscordIDs(ctx context.Context, lineNg
 					%s
 			) AS t(channel_id, guild_id, id, id_type) ON CONFLICT (channel_id, id, id_type) DO NOTHING
 		WHERE
-			line_ng_id.channel_id = t.channel_id AND
-			line_ng_id.id = t.id AND
-			line_ng_id.id_type = t.id_type
+			line_ng_discord_id.channel_id = t.channel_id AND
+			line_ng_discord_id.id = t.id AND
+			line_ng_discord_id.id_type = t.id_type
 	)
 	`, strings.Join(values, ","))
 	_, err := r.db.ExecContext(ctx, query)
--- web/handler/api/linechannel/internal/repository_test.go
+++ web/handler/api/linechannel/internal/repository_test.go
@@ -34,10 +34,10 @@ func TestRepository_UpdateLineBot(t *testing.T) {
 
 		repo := NewRepository(tx)
 		updateLineChannel := LineChannel{
-				ChannelID:  "123456789",
-				GuildID:    "987654321",
-				Ng:         true,
-				BotMessage: true,
+			ChannelID:  "123456789",
+			GuildID:    "987654321",
+			Ng:         true,
+			BotMessage: true,
 		}
 		err = repo.UpdateLinePostDiscordChannel(ctx, updateLineChannel)
 		assert.NoError(t, err)
@@ -92,7 +92,7 @@ func TestRepository_InsertLineNgDiscordMessageTypes(t *testing.T) {
 
 func TestRepository_DeleteLineNgDiscordMessageTypes(t *testing.T) {
 	ctx := context.Background()
-	t.Run("Channelが正しく削除されること", func(t *testing.T) {
+	t.Run("NGなメッセージタイプが正しく削除されること", func(t *testing.T) {
 		dbV1, cleanup, err := db.NewDB(ctx, config.DatabaseName(), config.DatabaseURL())
 		assert.NoError(t, err)
 		defer cleanup()
@@ -134,6 +134,104 @@ func TestRepository_DeleteLineNgDiscordMessageTypes(t *testing.T) {
 
 		var lineChannelCount int
 		err = tx.GetContext(ctx, &lineChannelCount, "SELECT COUNT(*) FROM line_post_discord_channel")
+		assert.NoError(t, err)
+
+		assert.Equal(t, 2, lineChannelCount)
+	})
+}
+
+func TestRepository_InsertLineNgDiscordIDs(t *testing.T) {
+	ctx := context.Background()
+	t.Run("NGなIDが正しく追加されること", func(t *testing.T) {
+		dbV1, cleanup, err := db.NewDB(ctx, config.DatabaseName(), config.DatabaseURL())
+		assert.NoError(t, err)
+		defer cleanup()
+		tx, err := dbV1.BeginTxx(ctx, nil)
+		assert.NoError(t, err)
+
+		defer tx.RollbackCtx(ctx)
+
+		repo := NewRepository(tx)
+		lineNgDiscordIDs := []LineNgID{
+			{
+				ChannelID: "123456789",
+				GuildID:   "987654321",
+				ID:        "123456789",
+				IDType:    "user",
+			},
+			{
+				ChannelID: "123456789",
+				GuildID:   "123456789",
+				ID:        "987654321",
+				IDType:    "user",
+			},
+			{
+				ChannelID: "987654321",
+				GuildID:   "123456789",
+				ID:        "987654321",
+				IDType:    "user",
+			},
+		}
+		err = repo.InsertLineNgDiscordIDs(ctx, lineNgDiscordIDs)
+		assert.NoError(t, err)
+
+		var lineChannelCount int
+		err = tx.GetContext(ctx, &lineChannelCount, "SELECT COUNT(*) FROM line_ng_discord_id WHERE channel_id = $1", "123456789")
+		assert.NoError(t, err)
+
+		assert.Equal(t, 3, lineChannelCount)
+	})
+}
+
+func TestRepository_DeleteLineNgDiscordIDs(t *testing.T) {
+	ctx := context.Background()
+	t.Run("NGなIDが正しく削除されること", func(t *testing.T) {
+		dbV1, cleanup, err := db.NewDB(ctx, config.DatabaseName(), config.DatabaseURL())
+		assert.NoError(t, err)
+		defer cleanup()
+		tx, err := dbV1.BeginTxx(ctx, nil)
+		assert.NoError(t, err)
+
+		defer tx.RollbackCtx(ctx)
+
+		f := &fixtures.Fixture{DBv1: tx}
+		f.Build(t,
+			fixtures.NewLineNgDiscordID(ctx, func(lnt *fixtures.LineNgDiscordID) {
+				lnt.ChannelID = "123456789"
+				lnt.ID = "123456789"
+				lnt.IDType = "user"
+			}),
+			fixtures.NewLineNgDiscordID(ctx, func(lnt *fixtures.LineNgDiscordID) {
+				lnt.ChannelID = "123456789"
+				lnt.ID = "987654321"
+				lnt.IDType = "user"
+			}),
+			fixtures.NewLineNgDiscordID(ctx, func(lnt *fixtures.LineNgDiscordID) {
+				lnt.ChannelID = "987654321"
+				lnt.ID = "123456789"
+				lnt.IDType = "user"
+			}),
+		)
+
+		repo := NewRepository(tx)
+		insertLineNgDiscordIDs := []LineNgID{
+			{
+				ChannelID: "123456789",
+				ID:        "123456789",
+				IDType:    "user",
+			},
+			{
+				ChannelID: "987654321",
+				ID:        "123456789",
+				IDType:    "user",
+			},
+		}
+
+		err = repo.DeleteNotInsertLineNgDiscordIDs(ctx, insertLineNgDiscordIDs)
+		assert.NoError(t, err)
+
+		var lineChannelCount int
+		err = tx.GetContext(ctx, &lineChannelCount, "SELECT COUNT(*) FROM line_ng_discord_id")
 		assert.NoError(t, err)
 
 		assert.Equal(t, 2, lineChannelCount)

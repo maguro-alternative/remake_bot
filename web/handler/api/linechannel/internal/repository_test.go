50d1e08a70b02551e823d96a6b4ffa4f1cece70a
ygo <ka.ra.si.naporitan@gmail.com>
2024-02-20 17:46:26 +0900

    テスト追加

--- web/handler/api/linechannel/internal/repository_test.go
+++ web/handler/api/linechannel/internal/repository_test.go
@@ -52,3 +52,90 @@ func TestRepository_UpdateLineBot(t *testing.T) {
 		assert.Equal(t, true, lineChannel.BotMessage)
 	})
 }
+
+func TestRepository_InsertLineNgDiscordMessageTypes(t *testing.T) {
+	ctx := context.Background()
+	t.Run("Channelが正しく追加されること", func(t *testing.T) {
+		dbV1, cleanup, err := db.NewDB(ctx, config.DatabaseName(), config.DatabaseURL())
+		assert.NoError(t, err)
+		defer cleanup()
+		tx, err := dbV1.BeginTxx(ctx, nil)
+		assert.NoError(t, err)
+
+		defer tx.RollbackCtx(ctx)
+
+		repo := NewRepository(tx)
+		lineNgDiscordTypes := []LineNgType{
+			{
+				ChannelID: "123456789",
+				Type:      6,
+			},
+			{
+				ChannelID: "123456789",
+				Type:      7,
+			},
+			{
+				ChannelID: "987654321",
+				Type:      6,
+			},
+		}
+		err = repo.InsertLineNgDiscordMessageTypes(ctx, lineNgDiscordTypes)
+		assert.NoError(t, err)
+
+		var lineChannelCount int
+		err = tx.GetContext(ctx, &lineChannelCount, "SELECT COUNT(*) FROM line_post_discord_channel WHERE channel_id = $1", "123456789")
+		assert.NoError(t, err)
+
+		assert.Equal(t, 3, lineChannelCount)
+	})
+}
+
+func TestRepository_DeleteLineNgDiscordMessageTypes(t *testing.T) {
+	ctx := context.Background()
+	t.Run("Channelが正しく削除されること", func(t *testing.T) {
+		dbV1, cleanup, err := db.NewDB(ctx, config.DatabaseName(), config.DatabaseURL())
+		assert.NoError(t, err)
+		defer cleanup()
+		tx, err := dbV1.BeginTxx(ctx, nil)
+		assert.NoError(t, err)
+
+		defer tx.RollbackCtx(ctx)
+
+		f := &fixtures.Fixture{DBv1: tx}
+		f.Build(t,
+			fixtures.NewLineNgType(ctx, func(lnt *fixtures.LineNgType) {
+				lnt.ChannelID = "123456789"
+				lnt.Type = 6
+			}),
+			fixtures.NewLineNgType(ctx, func(lnt *fixtures.LineNgType) {
+				lnt.ChannelID = "123456789"
+				lnt.Type = 7
+			}),
+			fixtures.NewLineNgType(ctx, func(lnt *fixtures.LineNgType) {
+				lnt.ChannelID = "987654321"
+				lnt.Type = 6
+			}),
+		)
+
+		repo := NewRepository(tx)
+		insertLineNgDiscordTypes := []LineNgType{
+			{
+				ChannelID: "123456789",
+				Type:      6,
+			},
+			{
+				ChannelID: "987654321",
+				Type:      6,
+			},
+		}
+
+		err = repo.DeleteNotInsertLineNgDiscordMessageTypes(ctx, insertLineNgDiscordTypes)
+		assert.NoError(t, err)
+
+		var lineChannelCount int
+		err = tx.GetContext(ctx, &lineChannelCount, "SELECT COUNT(*) FROM line_post_discord_channel")
+		assert.NoError(t, err)
+
+		assert.Equal(t, 2, lineChannelCount)
+	})
+}

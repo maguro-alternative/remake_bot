d513ae0761858e55fd112dcca38e7482d48a14f7
ygo <ka.ra.si.naporitan@gmail.com>
2024-02-21 16:37:22 +0900

    byteaテスト

--- pkg/db/db_test.go
+++ pkg/db/db_test.go
@@ -4,12 +4,12 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"time"
 	"testing"
+	"time"
 
-	_ "github.com/lib/pq"
-	"github.com/stretchr/testify/assert"
 	"github.com/joho/godotenv"
+	"github.com/lib/pq"
+	"github.com/stretchr/testify/assert"
 )
 
 func init() {
@@ -44,5 +44,43 @@ func TestNewDB(t *testing.T) {
 		_, cleanup, err := NewDB(ctx, "postgres", dbUri)
 		assert.Error(t, err)
 		defer cleanup()
+	})
+}
+
+func TestDB_Bytea(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	dbUri := fmt.Sprintf("%s://%s:%s/%s?user=%s&password=%s&sslmode=disable", os.Getenv("DB_NAME"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"))
+	db, cleanup, err := NewDB(ctx, "postgres", dbUri)
+	assert.NoError(t, err)
+	tx, err := db.BeginTxx(ctx, nil)
+	assert.NoError(t, err)
+	defer cleanup()
+	defer tx.RollbackCtx(ctx)
+
+	_, err = tx.ExecContext(ctx, "CREATE TABLE bytea_table (bytea_column BYTEA)")
+	assert.NoError(t, err)
+
+	t.Run("Bytea型のデータが正常に登録できること", func(t *testing.T) {
+		_, err := tx.ExecContext(ctx, "INSERT INTO bytea_table (bytea_column) VALUES ($1)", []byte{0x62, 0x79, 0x74, 0x65, 0x61})
+		assert.NoError(t, err)
+	})
+
+	t.Run("Bytea型のデータが正常に取得できること", func(t *testing.T) {
+		var row pq.ByteaArray
+		err := tx.SelectContext(ctx, &row, "SELECT bytea_column FROM bytea_table")
+		assert.NoError(t, err)
+
+		assert.Equal(t, []byte("bytea"), row[0])
+	})
+
+	t.Run("Bytea型のデータが正常に更新できること", func(t *testing.T) {
+		_, err := tx.ExecContext(ctx, "UPDATE bytea_table SET bytea_column = $1", []byte("bytea"))
+		assert.NoError(t, err)
+	})
+
+	t.Run("Bytea型のデータが正常に削除できること", func(t *testing.T) {
+		_, err := tx.ExecContext(ctx, "DELETE FROM bytea_table")
+		assert.NoError(t, err)
 	})
 }

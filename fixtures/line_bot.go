0660fe9b30c388efdd80f38cb4bf5faabcaa26dc
ygo <ka.ra.si.naporitan@gmail.com>
2024-02-21 18:53:46 +0900

    byteaに変更

--- fixtures/line_bot.go
+++ fixtures/line_bot.go
@@ -3,29 +3,34 @@ package fixtures
 import (
 	"context"
 	"testing"
+
+	"github.com/lib/pq"
 )
 
 type LineBot struct {
 	GuildID          string `db:"guild_id"`
-	LineNotifyToken  []byte `db:"line_notify_token"`
-	LineBotToken     []byte `db:"line_bot_token"`
-	LineBotSecret    []byte `db:"line_bot_secret"`
-	LineGroupID      []byte `db:"line_group_id"`
-	LineClientID     []byte `db:"line_client_id"`
-	LineClientSecret []byte `db:"line_client_secret"`
+	LineNotifyToken  pq.ByteaArray `db:"line_notify_token"`
+	LineBotToken     pq.ByteaArray `db:"line_bot_token"`
+	LineBotSecret    pq.ByteaArray `db:"line_bot_secret"`
+	LineGroupID      pq.ByteaArray `db:"line_group_id"`
+	LineClientID     pq.ByteaArray `db:"line_client_id"`
+	LineClientSecret pq.ByteaArray `db:"line_client_secret"`
 	DefaultChannelID string `db:"default_channel_id"`
 	DebugMode        bool   `db:"debug_mode"`
 }
 
 func NewLineBot(ctx context.Context, setter ...func(b *LineBot)) *ModelConnector {
+	bytes := make([]byte, 13)
+	copy(bytes, []byte("1111111111111"))
+
 	lineBot := &LineBot{
 		GuildID:          "1111111111111",
-		LineNotifyToken:  []byte("1111111111111"),
-		LineBotToken:     []byte("1111111111111"),
-		LineBotSecret:    []byte("1111111111111"),
-		LineGroupID:      []byte("1111111111111"),
-		LineClientID:     []byte("1111111111111"),
-		LineClientSecret: []byte("1111111111111"),
+		LineNotifyToken:  pq.ByteaArray{bytes},
+		LineBotToken:     pq.ByteaArray{bytes},
+		LineBotSecret:    pq.ByteaArray{bytes},
+		LineGroupID:      pq.ByteaArray{bytes},
+		LineClientID:     pq.ByteaArray{bytes},
+		LineClientSecret: pq.ByteaArray{bytes},
 		DefaultChannelID: "1111111111111",
 		DebugMode:        true,
 	}

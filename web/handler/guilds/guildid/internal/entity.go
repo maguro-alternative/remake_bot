3adc47bd5edab23ccddc951ce34449e3e74a45ed
ygo <ka.ra.si.naporitan@gmail.com>
2024-02-28 13:29:36 +0900

    guildid追加

--- web/handler/guilds/guildid/internal/entity.go
+++ web/handler/guilds/guildid/internal/entity.go
@@ -5,13 +5,14 @@ import (
 )
 
 type DiscordChannel struct {
+	ID       string `db:"id"`
 	Name     string `db:"name"`
 	Position int    `db:"position"`
 }
 
 type DiscordChannelSelect struct {
-	ID       string `db:"id"`
-	Name     string `db:"name"`
+	ID   string `db:"id"`
+	Name string `db:"name"`
 }
 
 type LineBot struct {
@@ -25,7 +26,7 @@ type LineBot struct {
 }
 
 type LineBotIv struct {
-	GuildID          string        `db:"guild_id"`
+	GuildID           string        `db:"guild_id"`
 	LineNotifyTokenIv pq.ByteaArray `db:"line_notify_token_iv"`
 	LineBotTokenIv    pq.ByteaArray `db:"line_bot_token_iv"`
 	LineBotSecretIv   pq.ByteaArray `db:"line_bot_secret_iv"`

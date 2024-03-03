0c215db770ff1cb4031f5b0e25995d5121da96da
ygo <ka.ra.si.naporitan@gmail.com>
2024-03-03 11:56:16 +0900

    json削除

--- pkg/line/line_notify.go
+++ pkg/line/line_notify.go
@@ -1,58 +1,13 @@
 package line
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
 	"net/url"
 	"strings"
-
-	validation "github.com/go-ozzo/ozzo-validation"
 )
-
-// LINE Notifyのメッセージ
-type LineNotifyMessage struct {
-	Message string `json:"message"`
-}
-
-func (l *LineNotifyMessage) Validate() error {
-	return validation.ValidateStruct(l,
-		validation.Field(&l.Message, validation.Required),
-	)
-}
-
-// LINE Notifyの画像
-type LineNotifyImage struct {
-	ImageThumbnail string `json:"imageThumbnail"`
-	ImageFullsize  string `json:"imageFullsize"`
-	Message        string `json:"message"`
-}
-
-func (l *LineNotifyImage) Validate() error {
-	return validation.ValidateStruct(l,
-		validation.Field(&l.ImageThumbnail, validation.Required),
-		validation.Field(&l.ImageFullsize, validation.Required),
-		validation.Field(&l.Message, validation.Required),
-	)
-}
-
-// LINE Notifyのスタンプ
-type LineNotifySticker struct {
-	StickerPackageID string `json:"stickerPackageId"`
-	StickerID        string `json:"stickerId"`
-	Message          string `json:"message"`
-}
-
-func (l *LineNotifySticker) Validate() error {
-	return validation.ValidateStruct(l,
-		validation.Field(&l.StickerPackageID, validation.Required),
-		validation.Field(&l.StickerID, validation.Required),
-		validation.Field(&l.Message, validation.Required),
-	)
-}
 
 // LINE Notifyのリクエスト
 type LineNotifyCall struct {
@@ -84,7 +39,8 @@ func (r *LineRequest) PushMessageNotify(ctx context.Context, message string) err
 		return err
 	}
 	if resp.StatusCode != http.StatusOK {
-		return errors.New(fmt.Sprintln("%+v\n", resp))
+		bodyStr := fmt.Sprintf("%+v\n", resp)
+		return errors.New(bodyStr)
 	}
 	defer resp.Body.Close()
 
@@ -94,21 +50,18 @@ func (r *LineRequest) PushMessageNotify(ctx context.Context, message string) err
 // LINE Notifyで画像を送信
 func (r *LineRequest) PushImageNotify(ctx context.Context, message, image string) error {
 	client := &http.Client{}
-	url := "https://notify-api.line.me/api/notify"
-	notifyMessage := LineNotifyImage{
-		Message:        message,
-		ImageThumbnail: image,
-		ImageFullsize:  image,
-	}
-	err := notifyMessage.Validate()
+	notifyUrl := "https://notify-api.line.me/api/notify"
+	u, err := url.ParseRequestURI(notifyUrl)
 	if err != nil {
 		return err
 	}
-	notifyMessageByte, err := json.Marshal(notifyMessage)
-	if err != nil {
-		return err
-	}
-	req, err := http.NewRequestWithContext(ctx, "POST", url, bytes.NewBuffer(notifyMessageByte))
+	form := url.Values{}
+	form.Add("message", message)
+	form.Add("imageThumbnail", image)
+	form.Add("imageFullsize", image)
+
+	body := strings.NewReader(form.Encode())
+	req, err := http.NewRequestWithContext(ctx, "POST", u.String(), body)
 	if err != nil {
 		return err
 	}
@@ -118,6 +71,10 @@ func (r *LineRequest) PushImageNotify(ctx context.Context, message, image string
 	resp, err := client.Do(req)
 	if err != nil {
 		return err
+	}
+	if resp.StatusCode != http.StatusOK {
+		bodyStr := fmt.Sprintf("%+v\n", resp)
+		return errors.New(bodyStr)
 	}
 	defer resp.Body.Close()
 
@@ -127,21 +84,18 @@ func (r *LineRequest) PushImageNotify(ctx context.Context, message, image string
 // LINE Notifyでスタンプを送信
 func (r *LineRequest) PushStampNotify(ctx context.Context, message, stickerPackageID, stickerID string) error {
 	client := &http.Client{}
-	url := "https://notify-api.line.me/api/notify"
-	notifyMessage := LineNotifySticker{
-		Message:          message,
-		StickerPackageID: stickerPackageID,
-		StickerID:        stickerID,
-	}
-	err := notifyMessage.Validate()
+	notifyUrl := "https://notify-api.line.me/api/notify"
+	u, err := url.ParseRequestURI(notifyUrl)
 	if err != nil {
 		return err
 	}
-	notifyMessageByte, err := json.Marshal(notifyMessage)
-	if err != nil {
-		return err
-	}
-	req, err := http.NewRequestWithContext(ctx, "POST", url, bytes.NewBuffer(notifyMessageByte))
+	form := url.Values{}
+	form.Add("message", message)
+	form.Add("stickerPackageId", stickerPackageID)
+	form.Add("stickerId", stickerID)
+
+	body := strings.NewReader(form.Encode())
+	req, err := http.NewRequestWithContext(ctx, "POST", u.String(), body)
 	if err != nil {
 		return err
 	}
@@ -152,6 +106,10 @@ func (r *LineRequest) PushStampNotify(ctx context.Context, message, stickerPacka
 	if err != nil {
 		return err
 	}
+	if resp.StatusCode != http.StatusOK {
+		bodyStr := fmt.Sprintf("%+v\n", resp)
+		return errors.New(bodyStr)
+	}
 	defer resp.Body.Close()
 
 	return nil
@@ -160,19 +118,16 @@ func (r *LineRequest) PushStampNotify(ctx context.Context, message, stickerPacka
 // LINE Notifyのメッセージを送信するためのリクエストを作成(実行はDo()で行う)
 func (r *LineRequest) PushMessageNotifyCall(ctx context.Context, message string) (*LineNotifyCall, error) {
 	client := &http.Client{}
-	url := "https://notify-api.line.me/api/notify"
-	notifyMessage := LineNotifyMessage{
-		Message: message,
-	}
-	err := notifyMessage.Validate()
+	notifyUrl := "https://notify-api.line.me/api/notify"
+	u, err := url.ParseRequestURI(notifyUrl)
 	if err != nil {
 		return nil, err
 	}
-	notifyMessageByte, err := json.Marshal(notifyMessage)
-	if err != nil {
-		return nil, err
-	}
-	req, err := http.NewRequestWithContext(ctx, "POST", url, bytes.NewBuffer(notifyMessageByte))
+	form := url.Values{}
+	form.Add("message", message)
+
+	body := strings.NewReader(form.Encode())
+	req, err := http.NewRequestWithContext(ctx, "POST", u.String(), body)
 	if err != nil {
 		return nil, err
 	}
@@ -188,25 +143,23 @@ func (r *LineRequest) PushMessageNotifyCall(ctx context.Context, message string)
 // LINE Notifyの画像を送信するためのリクエストを作成(実行はDo()で行う)
 func (r *LineRequest) PushImageNotifyCall(ctx context.Context, message, image string) (*LineNotifyCall, error) {
 	client := &http.Client{}
-	url := "https://notify-api.line.me/api/notify"
-	notifyMessage := LineNotifyImage{
-		Message:        message,
-		ImageThumbnail: image,
-		ImageFullsize:  image,
-	}
-	err := notifyMessage.Validate()
+	notifyUrl := "https://notify-api.line.me/api/notify"
+	u, err := url.ParseRequestURI(notifyUrl)
 	if err != nil {
 		return nil, err
 	}
-	notifyMessageByte, err := json.Marshal(notifyMessage)
-	if err != nil {
-		return nil, err
-	}
-	req, err := http.NewRequestWithContext(ctx, "POST", url, bytes.NewBuffer(notifyMessageByte))
+	form := url.Values{}
+	form.Add("message", message)
+	form.Add("imageThumbnail", image)
+	form.Add("imageFullsize", image)
+
+	body := strings.NewReader(form.Encode())
+	req, err := http.NewRequestWithContext(ctx, "POST", u.String(), body)
 	if err != nil {
 		return nil, err
 	}
 
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	req.Header.Set("Authorization", "Bearer "+r.lineNotifyToken)
 	return &LineNotifyCall{
 		c: client,
@@ -217,22 +170,18 @@ func (r *LineRequest) PushImageNotifyCall(ctx context.Context, message, image st
 // LINE Notifyのスタンプを送信するためのリクエストを作成(実行はDo()で行う)
 func (r *LineRequest) PushStampNotifyCall(ctx context.Context, message, stickerPackageID, stickerID string) (*LineNotifyCall, error) {
 	client := &http.Client{}
-	url := "https://notify-api.line.me/api/notify"
-	notifyMessage := LineNotifySticker{
-		Message:          message,
-		StickerPackageID: stickerPackageID,
-		StickerID:        stickerID,
-	}
-	err := notifyMessage.Validate()
+	notifyUrl := "https://notify-api.line.me/api/notify"
+	u, err := url.ParseRequestURI(notifyUrl)
 	if err != nil {
 		return nil, err
 	}
+	form := url.Values{}
+	form.Add("message", message)
+	form.Add("stickerPackageId", stickerPackageID)
+	form.Add("stickerId", stickerID)
 
-	notifyMessageByte, err := json.Marshal(notifyMessage)
-	if err != nil {
-		return nil, err
-	}
-	req, err := http.NewRequestWithContext(ctx, "POST", url, bytes.NewBuffer(notifyMessageByte))
+	body := strings.NewReader(form.Encode())
+	req, err := http.NewRequestWithContext(ctx, "POST", u.String(), body)
 	if err != nil {
 		return nil, err
 	}

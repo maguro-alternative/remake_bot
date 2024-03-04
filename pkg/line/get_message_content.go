c58bb56eac41227b8d6ba458f7d907c264157c43
ygo <ka.ra.si.naporitan@gmail.com>
2024-03-04 09:22:00 +0900

    closer削除

--- pkg/line/get_message_content.go
+++ pkg/line/get_message_content.go
@@ -2,6 +2,7 @@ package line
 
 import (
 	"context"
+	"errors"
 	"io"
 	"net/http"
 )
@@ -16,18 +17,21 @@ type LineMessageContent struct {
 func (r *LineRequest) GetContent(ctx context.Context, messageID string) (LineMessageContent, error) {
 	//var buf bytes.Buffer
 	client := &http.Client{}
-	url := "https://api.line.me/v2/bot/message/" + messageID + "/content"
+	url := "https://api-data.line.me/v2/bot/message/" + messageID + "/content"
 	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
 	if err != nil {
 		return LineMessageContent{}, err
 	}
 
+	req.Header.Set("Content-Type", "application/json; charset=UTF-8")
 	req.Header.Set("Authorization", "Bearer "+r.lineBotToken)
 	resp, err := client.Do(req)
 	if err != nil {
 		return LineMessageContent{}, err
 	}
-	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return LineMessageContent{}, errors.New("LINEメッセージの取得に失敗しました。" + resp.Status)
+	}
 	content := LineMessageContent{
 		Content:       resp.Body,
 		ContentType:   resp.Header.Get("Content-Type"),

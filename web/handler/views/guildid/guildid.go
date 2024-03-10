00b9f078cd284efb1e557d90e18f0a4a435dce67
ygo <ka.ra.si.naporitan@gmail.com>
2024-03-10 09:33:58 +0900

    認証の変更

--- web/handler/views/guildid/guildid.go
+++ web/handler/views/guildid/guildid.go
@@ -32,15 +32,15 @@ func (g *GuildIDViewHandler) Index(w http.ResponseWriter, r *http.Request) {
 	}
 	statusCode, err := permission.CheckDiscordPermission(ctx, w, r, g.IndexService, guild, "line_bot")
 	if err != nil {
+		if statusCode == 302 {
+			http.Redirect(w, r, "/auth/discord", http.StatusFound)
+			return
+		}
+		if statusCode != 200 {
+			http.Error(w, "Not get guild id", statusCode)
+			return
+		}
 		http.Error(w, "Not get guild id", http.StatusInternalServerError)
-		return
-	}
-	if statusCode == 302 {
-		http.Redirect(w, r, "/auth/discord", http.StatusFound)
-		return
-	}
-	if statusCode != 200 {
-		http.Error(w, "Not get guild id", statusCode)
 		return
 	}
 	tmpl := template.Must(template.ParseFiles("web/templates/views/guildid.html"))

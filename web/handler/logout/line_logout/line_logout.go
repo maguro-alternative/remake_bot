5ef660a770c9bdbaa2754a47f43fd9069d253d8f
ygo <ka.ra.si.naporitan@gmail.com>
2024-03-15 22:27:41 +0900

    lineログアウト追加

--- web/handler/logout/line_logout/line_logout.go
+++ web/handler/logout/line_logout/line_logout.go
@@ -1,4 +1,4 @@
-package discordlogout
+package linelogout
 
 import (
 	"context"
@@ -11,40 +11,41 @@ import (
 	"github.com/maguro-alternative/remake_bot/web/shared/session/model"
 )
 
-type DiscordOAuth2Handler struct {
-	DiscordOAuth2Service *service.DiscordOAuth2Service
+type LineLogoutHandler struct {
+	LineLogoutService *service.IndexService
 }
 
-func NewDiscordOAuth2Handler(discordOAuth2Service *service.DiscordOAuth2Service) *DiscordOAuth2Handler {
-	return &DiscordOAuth2Handler{
-		DiscordOAuth2Service: discordOAuth2Service,
+func NewLineLogoutHandler(lineLogoutService *service.IndexService) *LineLogoutHandler {
+	return &LineLogoutHandler{
+		LineLogoutService: lineLogoutService,
 	}
 }
 
-// Discordの認証情報を削除し、ログアウトする
-func (h *DiscordOAuth2Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// Lineの認証情報を削除し、ログアウトする
+func (h *LineLogoutHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// セッションに保存する構造体の型を登録
 	// これがない場合、エラーが発生する
 	ctx := r.Context()
 	if ctx == nil {
 		ctx = context.Background()
 	}
-	gob.Register(&model.DiscordUser{})
-	session, err := h.DiscordOAuth2Service.CookieStore.Get(r, config.SessionSecret())
+	gob.Register(&model.LineOAuthSession{})
+	session, err := h.LineLogoutService.CookieStore.Get(r, config.SessionSecret())
 	if err != nil {
 		slog.InfoContext(ctx, "sessionの取得に失敗しました。"+err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-	session.Values["discord_oauth_token"] = ""
-	session.Values["discord_user"] = model.DiscordUser{}
+	session.Values["line_oauth_token"] = ""
+	session.Values["guild_id"] = ""
+	session.Values["line_user"] = model.LineOAuthSession{}
 	err = session.Save(r, w)
 	if err != nil {
 		slog.InfoContext(ctx, "セッションの初期化に失敗しました。"+err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-	err = h.DiscordOAuth2Service.CookieStore.Save(r, w, session)
+	err = h.LineLogoutService.CookieStore.Save(r, w, session)
 	if err != nil {
 		slog.InfoContext(ctx, "セッションの初期化に失敗しました。"+err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)

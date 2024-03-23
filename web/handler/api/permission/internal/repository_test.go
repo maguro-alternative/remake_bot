979134cdb5c7db21b25e39234702446ce1c51a10
ygo <ka.ra.si.naporitan@gmail.com>
2024-03-23 17:53:45 +0900

    insertとdeleteのテスト追加

--- web/handler/api/permission/internal/repository_test.go
+++ web/handler/api/permission/internal/repository_test.go
@@ -50,3 +50,71 @@ func TestRepository_UpdatePermissionsCode(t *testing.T) {
 		assert.Equal(t, int64(9), permissionsCode.Code)
 	})
 }
+
+func TestRepository_DeletePermissionsID(t *testing.T) {
+	ctx := context.Background()
+	t.Run("Permissions_idが正しく削除されること", func(t *testing.T) {
+		dbV1, cleanup, err := db.NewDB(ctx, config.DatabaseName(), config.DatabaseURL())
+		assert.NoError(t, err)
+		defer cleanup()
+		tx, err := dbV1.BeginTxx(ctx, nil)
+		assert.NoError(t, err)
+
+		defer tx.RollbackCtx(ctx)
+
+		f := &fixtures.Fixture{DBv1: tx}
+		f.Build(t,
+			fixtures.NewPermissionsID(ctx, func(pi *fixtures.PermissionsID) {
+				pi.GuildID = "987654321"
+				pi.Type = "line_bot"
+				pi.TargetType = "user"
+				pi.TargetID = "123456789"
+				pi.Permission = "all"
+			}),
+		)
+
+		repo := NewRepository(tx)
+		err = repo.DeletePermissionIDs(ctx, "987654321")
+		assert.NoError(t, err)
+
+		var permissionsID PermissionID
+		err = tx.GetContext(ctx, &permissionsID, "SELECT * FROM permissions_id WHERE guild_id = $1", "987654321")
+		assert.Error(t, err)
+		assert.Empty(t, permissionsID)
+	})
+}
+
+func TestRepository_InsertPermissionsID(t *testing.T) {
+	ctx := context.Background()
+	t.Run("Permissions_idが正しく挿入されること", func(t *testing.T) {
+		dbV1, cleanup, err := db.NewDB(ctx, config.DatabaseName(), config.DatabaseURL())
+		assert.NoError(t, err)
+		defer cleanup()
+		tx, err := dbV1.BeginTxx(ctx, nil)
+		assert.NoError(t, err)
+
+		defer tx.RollbackCtx(ctx)
+
+		repo := NewRepository(tx)
+		insertPermissionsID := []PermissionID{
+			{
+				GuildID:    "987654321",
+				Type:       "line_bot",
+				TargetType: "user",
+				TargetID:   "123456789",
+				Permission: "all",
+			},
+		}
+		err = repo.InsertPermissionIDs(ctx, insertPermissionsID)
+		assert.NoError(t, err)
+
+		var permissionsID PermissionID
+		err = tx.GetContext(ctx, &permissionsID, "SELECT * FROM permissions_id WHERE guild_id = $1", "987654321")
+		assert.NoError(t, err)
+		assert.Equal(t, "987654321", permissionsID.GuildID)
+		assert.Equal(t, "line_bot", permissionsID.Type)
+		assert.Equal(t, "user", permissionsID.TargetType)
+		assert.Equal(t, "123456789", permissionsID.TargetID)
+		assert.Equal(t, "all", permissionsID.Permission)
+	})
+}

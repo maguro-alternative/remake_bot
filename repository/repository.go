84298ef222f9234e6e3111d103615577f262480f
ygo <ka.ra.si.naporitan@gmail.com>
2024-03-26 23:07:05 +0900

    repository追加

---
+++ repository/repository.go
@@ -0,0 +1,15 @@
+package repository
+
+import (
+	"github.com/maguro-alternative/remake_bot/pkg/db"
+)
+
+type Repository struct {
+	db db.Driver
+}
+
+func NewRepository(db db.Driver) *Repository {
+	return &Repository{
+		db: db,
+	}
+}

58f844b2c5ea4642e3c16f7471a284b05e07eb3b
ygo <ka.ra.si.naporitan@gmail.com>
2024-04-13 09:12:15 +0900

    session読み取り追加

---
+++ web/shared/session/session.go
@@ -0,0 +1,38 @@
+package session
+
+import (
+	"net/http"
+
+	"github.com/gorilla/sessions"
+)
+
+type SessionStore struct {
+	session *sessions.Session
+	store   *sessions.CookieStore
+}
+
+func NewSessionStore(
+	r *http.Request,
+	secret string,
+) *SessionStore {
+	store := sessions.NewCookieStore([]byte(secret))
+	session, err := store.Get(r, secret)
+	if err != nil {
+		return nil
+	}
+	return &SessionStore{
+		session: session,
+		store:   store,
+	}
+}
+
+func (s *SessionStore) SessionSave(w http.ResponseWriter) error {
+	return s.session.Save(nil, w)
+}
+
+func (s *SessionStore) CookieSave(
+	r *http.Request,
+	w http.ResponseWriter,
+) error {
+	return s.store.Save(r, w, s.session)
+}

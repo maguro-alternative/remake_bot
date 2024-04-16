8899306f588aa084eeeb9015a1151b1656e7fb07
ygo <ka.ra.si.naporitan@gmail.com>
2024-04-16 19:40:22 +0900

    コマンドテスト追加

--- bot/commands/command_handler_test.go
+++ bot/commands/command_handler_test.go
@@ -28,5 +28,40 @@ func Test_PingCommandRegister(t *testing.T) {
 		h := newCommandHandler(&session, state, "")
 		err := h.commandRegister(PingCommand(repo))
 		assert.NoError(t, err)
+		c := h.getCommands()
+		assert.NotNil(t, c)
+		assert.Equal(t, 1, len(c))
+		assert.Equal(t, "ping", c[0].Name)
+	})
+}
+
+func Test_PingCommandRemove(t *testing.T) {
+	repo := &repository.RepositoryFuncMock{}
+	session := mock.SessionMock{
+		AddHandlerFunc: func(interface{}) func() {
+			return func() {}
+		},
+		ApplicationCommandCreateFunc: func(guildID string, appID string, appCmd *discordgo.ApplicationCommand, options ...discordgo.RequestOption) (ccmd *discordgo.ApplicationCommand, err error) {
+			return appCmd, nil
+		},
+	}
+	t.Run("ping削除成功", func(t *testing.T){
+		state := discordgo.NewState()
+		state.User = &discordgo.User{
+			ID: "1234567890",
+		}
+		h := newCommandHandler(&session, state, "")
+		err := h.commandRegister(PingCommand(repo))
+		assert.NoError(t, err)
+		c := h.getCommands()
+		assert.NotNil(t, c)
+		assert.Equal(t, 1, len(c))
+		assert.Equal(t, "ping", c[0].Name)
+
+		err = h.commandRemove(c[0])
+		assert.NoError(t, err)
+
+		c = h.getCommands()
+		assert.Len(t, c, 0)
 	})
 }

2d37f457c78dad2ddf90cc9bb20b51621b6c7afa
ygo <ka.ra.si.naporitan@gmail.com>
2024-04-21 08:41:26 +0900

    discordログアウトのテスト追加

---
+++ web/handler/logout/discord_logout/discord_logout_test.go
@@ -0,0 +1,65 @@
+package discordlogout
+
+import (
+	//"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/maguro-alternative/remake_bot/web/config"
+	"github.com/maguro-alternative/remake_bot/web/service"
+	"github.com/maguro-alternative/remake_bot/web/shared/model"
+	"github.com/maguro-alternative/remake_bot/web/shared/session"
+
+	"github.com/gorilla/sessions"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestServeHTTP(t *testing.T) {
+    t.Run("Test with valid context", func(t *testing.T) {
+		cookieStore := sessions.NewCookieStore([]byte("test"))
+		indexService := &service.IndexService{
+			CookieStore: cookieStore,
+		} // Mock this service as per your implementation
+		handler := NewDiscordOAuth2Handler(indexService)
+
+        req, err := http.NewRequest("GET", "/logout/discord", nil)
+        assert.NoError(t, err)
+
+        rr := httptest.NewRecorder()
+        handlerFunc := http.HandlerFunc(handler.ServeHTTP)
+
+		sessionStore, err := session.NewSessionStore(req, cookieStore, config.SessionSecret())
+		require.NoError(t, err)
+		sessionStore.SetDiscordUser(&model.DiscordUser{
+			ID: "123",
+			Username: "test",
+		})
+		sessionStore.SetDiscordOAuthToken("aaa")
+
+        handlerFunc.ServeHTTP(rr, req)
+
+        assert.Equal(t, http.StatusSeeOther, rr.Code, "Expected response code to be 303")
+    })
+
+    t.Run("Test with Internal Server Error", func(t *testing.T) {
+		cookieStore := sessions.NewCookieStore([]byte(""))
+		indexService := &service.IndexService{
+			CookieStore: cookieStore,
+		} // Mock this service as per your implementation
+		handler := NewDiscordOAuth2Handler(indexService)
+
+        req, err := http.NewRequest("GET", "/logout/discord", nil)
+        assert.NoError(t, err)
+
+        rr := httptest.NewRecorder()
+        handlerFunc := http.HandlerFunc(handler.ServeHTTP)
+
+        handlerFunc.ServeHTTP(rr, req)
+
+        assert.Equal(t, http.StatusInternalServerError, rr.Code)
+    })
+
+    // Add more tests for different scenarios, for example when session.NewSessionStore returns an error
+}

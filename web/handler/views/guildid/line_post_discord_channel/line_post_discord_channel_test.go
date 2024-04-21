0974b399114318b267c5f93f5472761b8ac54195
ygo <ka.ra.si.naporitan@gmail.com>
2024-04-21 16:17:26 +0900

    line_post_discord_channel_testのビューテスト作成

---
+++ web/handler/views/guildid/line_post_discord_channel/line_post_discord_channel_test.go
@@ -0,0 +1,110 @@
+package linepostdiscordchannel
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/maguro-alternative/remake_bot/repository"
+
+	"github.com/maguro-alternative/remake_bot/web/service"
+	"github.com/maguro-alternative/remake_bot/web/shared/ctxvalue"
+	"github.com/maguro-alternative/remake_bot/web/shared/model"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewLinePostDiscordChannelViewHandler(t *testing.T) {
+	cwd, err := os.Getwd()
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		require.NoError(t, os.Chdir(cwd))
+	})
+	require.NoError(t, os.Chdir("../../../../../"))
+	t.Run("test new line post discord channel view handler", func(t *testing.T) {
+		indexService := &service.IndexService{
+			DiscordSession: &discordgo.Session{},
+		}
+		indexService.DiscordBotState = discordgo.NewState()
+		err := indexService.DiscordBotState.GuildAdd(&discordgo.Guild{
+			ID: "123",
+			Channels: []*discordgo.Channel{
+				{
+					ID:       "123",
+					Name:     "test",
+					Position: 1,
+					Type:     discordgo.ChannelTypeGuildText,
+				},
+				{
+					ID:       "1234",
+					Name:     "test",
+					Position: 2,
+					Type:     discordgo.ChannelTypeGuildText,
+				},
+				{
+					ID:       "12345",
+					Name:     "test",
+					Position: 3,
+					Type:     discordgo.ChannelTypeGuildText,
+				},
+			},
+			Members: []*discordgo.Member{
+				{
+					User: &discordgo.User{
+						ID: "123",
+					},
+				},
+			},
+		})
+		assert.NoError(t, err)
+		assert.Len(t, indexService.DiscordBotState.Guilds, 1)
+
+		mux := http.NewServeMux()
+
+		repo := &repository.RepositoryFuncMock{
+			GetLinePostDiscordChannelFunc: func(ctx context.Context, channelID string) (repository.LinePostDiscordChannel, error) {
+				return repository.LinePostDiscordChannel{
+					Ng: 		true,
+					BotMessage: false,
+				}, nil
+			},
+			GetLineNgDiscordMessageTypeFunc: func(ctx context.Context, channelID string) ([]int, error) {
+				return []int{}, nil
+			},
+			GetLineNgDiscordUserIDFunc: func(ctx context.Context, channelID string) ([]string, error) {
+				return []string{}, nil
+			},
+			GetLineNgDiscordRoleIDFunc: func(ctx context.Context, channelID string) ([]string, error) {
+				return []string{}, nil
+			},
+		}
+		handler := NewLinePostDiscordChannelViewHandler(indexService, repo)
+
+		mux.HandleFunc("/guilds/{guildId}/line_post_discord_channel", handler.Index)
+
+		req := httptest.NewRequest(http.MethodGet, "/guilds/123/line_post_discord_channel", nil)
+		rec := httptest.NewRecorder()
+
+		mux.ServeHTTP(rec, setCtxValue(req))
+
+		assert.Equal(t, http.StatusOK, rec.Code)
+	})
+}
+
+func setCtxValue(r *http.Request) *http.Request {
+	ctx := r.Context()
+	ctx = ctxvalue.ContextWithDiscordPermission(ctx, &model.DiscordPermissionData{
+		PermissionCode: 8,
+		User: 		 model.DiscordUser{
+			ID: "123",
+			Username: "test",
+			Avatar: "test",
+		},
+		Permission: "all",
+	})
+	return r.WithContext(ctx)
+}

b68da7de479e83b1f11ed10ccf60866e484ccf52
ygo <ka.ra.si.naporitan@gmail.com>
2024-04-21 15:44:29 +0900

    htmlの中身も確認

--- web/handler/views/group/group_test.go
+++ web/handler/views/group/group_test.go
@@ -31,27 +31,27 @@ func TestNewLineGroupViewHandler(t *testing.T) {
 		}
 		indexService.DiscordBotState = discordgo.NewState()
 		err := indexService.DiscordBotState.GuildAdd(&discordgo.Guild{
-				ID: "123",
-				Channels: []*discordgo.Channel{
-					{
-						ID: "123",
-						Name: "test",
-						Position: 1,
-						Type: discordgo.ChannelTypeGuildText,
-					},
-					{
-						ID: "1234",
-						Name: "test",
-						Position: 2,
-						Type: discordgo.ChannelTypeGuildText,
-					},
-					{
-						ID: "12345",
-						Name: "test",
-						Position: 3,
-						Type: discordgo.ChannelTypeGuildText,
-					},
+			ID: "123",
+			Channels: []*discordgo.Channel{
+				{
+					ID:       "123",
+					Name:     "test",
+					Position: 1,
+					Type:     discordgo.ChannelTypeGuildText,
 				},
+				{
+					ID:       "1234",
+					Name:     "test",
+					Position: 2,
+					Type:     discordgo.ChannelTypeGuildText,
+				},
+				{
+					ID:       "12345",
+					Name:     "test",
+					Position: 3,
+					Type:     discordgo.ChannelTypeGuildText,
+				},
+			},
 		})
 		assert.NoError(t, err)
 		assert.Len(t, indexService.DiscordBotState.Guilds, 1)
@@ -61,7 +61,102 @@ func TestNewLineGroupViewHandler(t *testing.T) {
 		repo := &repository.RepositoryFuncMock{
 			GetAllColumnsLineBotFunc: func(ctx context.Context, guildID string) (repository.LineBot, error) {
 				return repository.LineBot{
-					GuildID: "123",
+					GuildID:          "123",
+					DefaultChannelID: "123",
+				}, nil
+			},
+		}
+		handler := NewLineGroupViewHandler(indexService, repo)
+
+		_, err = handler.IndexService.DiscordBotState.Guild("123")
+		assert.NoError(t, err)
+
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodGet, "/group/123", nil)
+
+		mux.HandleFunc("/group/{guildId}", handler.Index)
+		mux.ServeHTTP(w, setCtxValue(r))
+
+		assert.Equal(t, http.StatusOK, w.Code)
+
+		assert.Contains(t, w.Body.String(), `<option value="123" selected>カテゴリーなし:📝:test</option>`)
+		assert.Contains(t, w.Body.String(), `<option value="1234">カテゴリーなし:📝:test</option>`)
+		assert.Contains(t, w.Body.String(), `<option value="12345">カテゴリーなし:📝:test</option>`)
+	})
+
+	t.Run("test new line group view handler with error", func(t *testing.T) {
+		indexService := &service.IndexService{
+			DiscordSession: &discordgo.Session{},
+		}
+		indexService.DiscordBotState = discordgo.NewState()
+		err := indexService.DiscordBotState.GuildAdd(&discordgo.Guild{
+			ID: "123",
+			Channels: []*discordgo.Channel{
+				{
+					ID:       "123",
+					Name:     "test",
+					Position: 1,
+					Type:     discordgo.ChannelTypeGuildText,
+				},
+				{
+					ID:       "1234",
+					Name:     "test",
+					Position: 2,
+					Type:     discordgo.ChannelTypeGuildText,
+				},
+				{
+					ID:       "12345",
+					Name:     "test",
+					Position: 3,
+					Type:     discordgo.ChannelTypeGuildText,
+				},
+			},
+		})
+		assert.NoError(t, err)
+		assert.Len(t, indexService.DiscordBotState.Guilds, 1)
+
+		mux := http.NewServeMux()
+
+		repo := &repository.RepositoryFuncMock{
+			GetAllColumnsLineBotFunc: func(ctx context.Context, guildID string) (repository.LineBot, error) {
+				return repository.LineBot{
+					GuildID:          "123",
+					DefaultChannelID: "123",
+				}, nil
+			},
+		}
+		handler := NewLineGroupViewHandler(indexService, repo)
+
+		_, err = handler.IndexService.DiscordBotState.Guild("123")
+		assert.NoError(t, err)
+
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodGet, "/group/123", nil)
+
+		mux.HandleFunc("/group/{guildId}", handler.Index)
+		mux.ServeHTTP(w, r)
+
+		assert.Equal(t, http.StatusFound, w.Code)
+	})
+
+	t.Run("test new line group view handler with error", func(t *testing.T) {
+		indexService := &service.IndexService{
+			DiscordSession: &discordgo.Session{},
+		}
+		indexService.DiscordBotState = discordgo.NewState()
+		err := indexService.DiscordBotState.GuildAdd(&discordgo.Guild{
+			ID: "123",
+			Channels: []*discordgo.Channel{},
+		})
+		assert.NoError(t, err)
+		assert.Len(t, indexService.DiscordBotState.Guilds, 1)
+
+		mux := http.NewServeMux()
+
+		repo := &repository.RepositoryFuncMock{
+			GetAllColumnsLineBotFunc: func(ctx context.Context, guildID string) (repository.LineBot, error) {
+				return repository.LineBot{
+					GuildID:          "123",
 					DefaultChannelID: "123",
 				}, nil
 			},
@@ -85,20 +180,20 @@ func setCtxValue(r *http.Request) *http.Request {
 	ctx := r.Context()
 	ctx = ctxvalue.ContextWithLineUser(ctx, &model.LineOAuthSession{
 		User: model.LineIdTokenUser{
-			Iss: "test",
-			Sub: "test",
-			Aud: "test",
-			Exp: 1,
-			Iat: 1,
+			Iss:      "test",
+			Sub:      "test",
+			Aud:      "test",
+			Exp:      1,
+			Iat:      1,
 			AuthTime: 1,
-			Nonce: "test",
-			Amr: []string{"test"},
-			Name: "test",
-			Picture: "test",
-			Email: "test",
+			Nonce:    "test",
+			Amr:      []string{"test"},
+			Name:     "test",
+			Picture:  "test",
+			Email:    "test",
 		},
 		DiscordGuildID: "123",
-		Token: "test",
+		Token:          "test",
 	})
 	return r.WithContext(ctx)
 }

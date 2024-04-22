64de098bdb70d0d6a5692edd7bfb8063658956f2
ygo <ka.ra.si.naporitan@gmail.com>
2024-04-23 08:37:07 +0900

    ロールのチェック追加

--- web/middleware/discord_oauth_check_test.go
+++ web/middleware/discord_oauth_check_test.go
@@ -690,6 +690,7 @@ func TestDiscordOAuthCheckMiddleware(t *testing.T) {
 					User: &discordgo.User{
 						ID: "123",
 					},
+					Roles: []string{"123"},
 				},
 			},
 		})
@@ -710,6 +711,121 @@ func TestDiscordOAuthCheckMiddleware(t *testing.T) {
 				},
 				GetPermissionRoleIDsFunc: func(ctx context.Context, guildID string, permissionType string) ([]repository.PermissionRoleID, error) {
 					return nil, nil
+				},
+			},
+			true,
+		)
+
+		mux := http.NewServeMux()
+
+		mux.HandleFunc("/guild/{guildId}/linetoken", middleware(handler).ServeHTTP)
+
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodGet, "/guild/111/linetoken", nil)
+
+		sessionStore, err := session.NewSessionStore(r, cookieStore, config.SessionSecret())
+		require.NoError(t, err)
+		sessionStore.SetDiscordUser(&user)
+		sessionStore.SetDiscordOAuthToken("test")
+		sessionStore.SessionSave(r, w)
+
+		defer sessionStore.CleanupDiscordUser()
+		defer sessionStore.CleanupDiscordOAuthToken()
+		defer sessionStore.SessionSave(r, w)
+
+		mux.ServeHTTP(w, r)
+		assert.Equal(t, http.StatusOK, w.Code)
+	})
+
+	t.Run("'/guild/{guildid}/linetoken'で正常に動作すること(許可ロール)", func(t *testing.T) {
+		handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		})
+		indexService := service.IndexService{
+			Client: newStubHttpClient(func(req *http.Request) *http.Response {
+				return &http.Response{
+					StatusCode: http.StatusOK,
+					Body: io.NopCloser(strings.NewReader(`{
+						"id": "123456789",
+						"username": "test",
+						"global_name": "test",
+						"avatar": "test",
+						"avatar_decoration": "test",
+						"discriminator": "1234",
+						"public_flags": 0,
+						"flags": 0,
+						"banner": "test",
+						"banner_color": "test",
+						"accent_color": "test",
+						"locale": "test",
+						"mfa_enabled": true,
+						"premium_type": 0,
+						"email": "test",
+						"verified": true,
+						"bio": "test",
+					}`)),
+				}
+			}),
+			CookieStore: sessions.NewCookieStore([]byte(config.SessionSecret())),
+			DiscordSession: &mock.SessionMock{
+				UserChannelPermissionsFunc: func(userID string, channelID string, fetchOptions ...discordgo.RequestOption) (apermissions int64, err error) {
+					return 0, nil
+				},
+			},
+		}
+		indexService.DiscordBotState = discordgo.NewState()
+		err := indexService.DiscordBotState.GuildAdd(&discordgo.Guild{
+			ID: "111",
+			Channels: []*discordgo.Channel{
+				{
+					ID:       "123",
+					Name:     "test",
+					Position: 1,
+					Type:     discordgo.ChannelTypeGuildText,
+				},
+				{
+					ID:       "1234",
+					Name:     "test",
+					Position: 2,
+					Type:     discordgo.ChannelTypeGuildText,
+				},
+				{
+					ID:       "12345",
+					Name:     "test",
+					Position: 3,
+					Type:     discordgo.ChannelTypeGuildText,
+				},
+			},
+			Members: []*discordgo.Member{
+				{
+					User: &discordgo.User{
+						ID: "123",
+					},
+				},
+			},
+			Roles: []*discordgo.Role{
+				{
+					ID:       "123",
+				},
+			},
+		})
+		require.NoError(t, err)
+		middleware := DiscordOAuthCheckMiddleware(
+			indexService,
+			&repository.RepositoryFuncMock{
+				GetPermissionCodeFunc: func(ctx context.Context, guildID string, permissionType string) (int64, error) {
+					return 8, nil
+				},
+				GetPermissionUserIDsFunc: func(ctx context.Context, guildID string, permissionType string) ([]repository.PermissionUserID, error) {
+					return nil, nil
+				},
+				GetPermissionRoleIDsFunc: func(ctx context.Context, guildID string, permissionType string) ([]repository.PermissionRoleID, error) {
+					return []repository.PermissionRoleID{
+						{
+							TargetID:   "123",
+							Permission: "write",
+						},
+					}, nil
 				},
 			},
 			true,

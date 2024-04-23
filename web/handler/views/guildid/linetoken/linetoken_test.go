2120ed3146b5bcbd2bf62493973b29da9077cede
ygo <ka.ra.si.naporitan@gmail.com>
2024-04-24 08:02:44 +0900

    入力済みかどうかテスト

--- web/handler/views/guildid/linetoken/linetoken_test.go
+++ web/handler/views/guildid/linetoken/linetoken_test.go
@@ -27,7 +27,7 @@ func TestNewLinePostDiscordChannelViewHandler(t *testing.T) {
 		require.NoError(t, os.Chdir(cwd))
 	})
 	require.NoError(t, os.Chdir("../../../../../"))
-	t.Run("正常に表示されるか", func(t *testing.T) {
+	t.Run("linetokenが正常に表示されるか", func(t *testing.T) {
 		indexService := &service.IndexService{
 			DiscordSession: &discordgo.Session{},
 		}
@@ -107,6 +107,165 @@ func TestNewLinePostDiscordChannelViewHandler(t *testing.T) {
 		assert.Contains(t, rec.Body.String(), `<option value="1234">カテゴリーなし:📝:test</option>`)
 		assert.Contains(t, rec.Body.String(), `<option value="12345">カテゴリーなし:📝:test</option>`)
 	})
+
+	t.Run("linetokenが正常に表示されるか(デフォルトチャンネルが選択済みになるか)", func(t *testing.T) {
+		indexService := &service.IndexService{
+			DiscordSession: &discordgo.Session{},
+		}
+		indexService.DiscordBotState = discordgo.NewState()
+		err := indexService.DiscordBotState.GuildAdd(&discordgo.Guild{
+			ID: "123",
+			Channels: []*discordgo.Channel{
+				{
+					ID:       "123",
+					Name:     "test",
+					Position: 1,
+					Type:     discordgo.ChannelTypeGuildText,
+				},
+				{
+					ID:       "1234",
+					Name:     "test",
+					Position: 2,
+					Type:     discordgo.ChannelTypeGuildText,
+				},
+				{
+					ID:       "12345",
+					Name:     "test",
+					Position: 3,
+					Type:     discordgo.ChannelTypeGuildText,
+				},
+			},
+			Members: []*discordgo.Member{
+				{
+					User: &discordgo.User{
+						ID: "123",
+					},
+				},
+			},
+		})
+		assert.NoError(t, err)
+		assert.Len(t, indexService.DiscordBotState.Guilds, 1)
+
+		mux := http.NewServeMux()
+
+		repo := &repository.RepositoryFuncMock{
+			GetAllColumnsLineBotFunc: func(ctx context.Context, guildId string) (repository.LineBot, error) {
+				return repository.LineBot{
+					GuildID:          "111",
+					LineNotifyToken:  pq.ByteaArray{[]byte("test")},
+					LineBotToken:     pq.ByteaArray{[]byte("test")},
+					LineBotSecret:    pq.ByteaArray{[]byte("test")},
+					LineGroupID:      pq.ByteaArray{[]byte("test")},
+					LineClientID:     pq.ByteaArray{[]byte("test")},
+					LineClientSecret: pq.ByteaArray{[]byte("test")},
+					DefaultChannelID: "123",
+				}, nil
+			},
+		}
+		handler := NewLineTokenViewHandler(indexService, repo)
+
+		mux.HandleFunc("/guilds/{guildId}/linetoken", handler.Index)
+
+		req := httptest.NewRequest(http.MethodGet, "/guilds/123/linetoken", nil)
+		rec := httptest.NewRecorder()
+
+		mux.ServeHTTP(rec, setCtxValue(req))
+
+		assert.Equal(t, http.StatusOK, rec.Code)
+
+		assert.Contains(t, rec.Body.String(), `<p>Discordアカウント: test</p>`)
+		assert.Contains(t, rec.Body.String(), `<img src="https://cdn.discordapp.com/avatars/123/test.webp?size=64" alt="Discordアイコン">`)
+		assert.Contains(t, rec.Body.String(), `<p>LINEアカウント: 未ログイン</p>`)
+
+		assert.Contains(t, rec.Body.String(), `<input id="lineNotifyToken" type="password" name="line_notify_token">`)
+		assert.Contains(t, rec.Body.String(), `<input id="lineBotToken" type="password" name="line_bot_token">`)
+		assert.Contains(t, rec.Body.String(), `<input id="lineBotSecret" type="password" name="line_bot_secret">`)
+		assert.Contains(t, rec.Body.String(), `<input id="lineGroupId" type="password" name="line_group_id">`)
+		assert.Contains(t, rec.Body.String(), `<input id="lineClientId" type="password" name="line_client_id">`)
+		assert.Contains(t, rec.Body.String(), `<input id="lineClientSecret" type="password" name="line_client_secret">`)
+		assert.Equal(t, strings.Count(rec.Body.String(),"入力済み"), 6)
+
+		assert.Contains(t, rec.Body.String(), `<option value="123" selected>カテゴリーなし:📝:test</option>`)
+		assert.Contains(t, rec.Body.String(), `<option value="1234">カテゴリーなし:📝:test</option>`)
+		assert.Contains(t, rec.Body.String(), `<option value="12345">カテゴリーなし:📝:test</option>`)
+	})
+
+	t.Run("linetokenが正常に表示されるか(入力済みが0か)", func(t *testing.T) {
+		indexService := &service.IndexService{
+			DiscordSession: &discordgo.Session{},
+		}
+		indexService.DiscordBotState = discordgo.NewState()
+		err := indexService.DiscordBotState.GuildAdd(&discordgo.Guild{
+			ID: "123",
+			Channels: []*discordgo.Channel{
+				{
+					ID:       "123",
+					Name:     "test",
+					Position: 1,
+					Type:     discordgo.ChannelTypeGuildText,
+				},
+				{
+					ID:       "1234",
+					Name:     "test",
+					Position: 2,
+					Type:     discordgo.ChannelTypeGuildText,
+				},
+				{
+					ID:       "12345",
+					Name:     "test",
+					Position: 3,
+					Type:     discordgo.ChannelTypeGuildText,
+				},
+			},
+			Members: []*discordgo.Member{
+				{
+					User: &discordgo.User{
+						ID: "123",
+					},
+				},
+			},
+		})
+		assert.NoError(t, err)
+		assert.Len(t, indexService.DiscordBotState.Guilds, 1)
+
+		mux := http.NewServeMux()
+
+		repo := &repository.RepositoryFuncMock{
+			GetAllColumnsLineBotFunc: func(ctx context.Context, guildId string) (repository.LineBot, error) {
+				return repository.LineBot{
+					GuildID:          "111",
+				}, nil
+			},
+		}
+		handler := NewLineTokenViewHandler(indexService, repo)
+
+		mux.HandleFunc("/guilds/{guildId}/linetoken", handler.Index)
+
+		req := httptest.NewRequest(http.MethodGet, "/guilds/123/linetoken", nil)
+		rec := httptest.NewRecorder()
+
+		mux.ServeHTTP(rec, setCtxValue(req))
+
+		assert.Equal(t, http.StatusOK, rec.Code)
+
+		assert.Contains(t, rec.Body.String(), `<p>Discordアカウント: test</p>`)
+		assert.Contains(t, rec.Body.String(), `<img src="https://cdn.discordapp.com/avatars/123/test.webp?size=64" alt="Discordアイコン">`)
+		assert.Contains(t, rec.Body.String(), `<p>LINEアカウント: 未ログイン</p>`)
+
+		assert.Contains(t, rec.Body.String(), `<input id="lineNotifyToken" type="password" name="line_notify_token">`)
+		assert.Contains(t, rec.Body.String(), `<input id="lineBotToken" type="password" name="line_bot_token">`)
+		assert.Contains(t, rec.Body.String(), `<input id="lineBotSecret" type="password" name="line_bot_secret">`)
+		assert.Contains(t, rec.Body.String(), `<input id="lineGroupId" type="password" name="line_group_id">`)
+		assert.Contains(t, rec.Body.String(), `<input id="lineClientId" type="password" name="line_client_id">`)
+		assert.Contains(t, rec.Body.String(), `<input id="lineClientSecret" type="password" name="line_client_secret">`)
+		assert.Equal(t, strings.Count(rec.Body.String(),"入力済み"), 0)
+
+		assert.Contains(t, rec.Body.String(), `<option value="123">カテゴリーなし:📝:test</option>`)
+		assert.Contains(t, rec.Body.String(), `<option value="1234">カテゴリーなし:📝:test</option>`)
+		assert.Contains(t, rec.Body.String(), `<option value="12345">カテゴリーなし:📝:test</option>`)
+	})
+
+
 }
 
 func setCtxValue(r *http.Request) *http.Request {

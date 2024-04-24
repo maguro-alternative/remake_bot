9035a3c0898c4d8c87ff212312d703df6addaac1
ygo <ka.ra.si.naporitan@gmail.com>
2024-04-24 12:54:08 +0900

    discordとlineのログインが反映されるか確認

--- web/handler/views/index_test.go
+++ web/handler/views/index_test.go
@@ -21,12 +21,15 @@ func TestNewLinePostDiscordChannelViewHandler(t *testing.T) {
 	t.Cleanup(func() {
 		require.NoError(t, os.Chdir(cwd))
 	})
-	require.NoError(t, os.Chdir("../../../../../"))
-	t.Run("正常に表示される", func(t *testing.T) {
+	require.NoError(t, os.Chdir("../../../"))
+	t.Run("ルートページが正常に表示される(discordログイン中)", func(t *testing.T) {
 		indexService := &service.IndexService{
 			DiscordSession: &discordgo.Session{},
 		}
 		indexService.DiscordBotState = discordgo.NewState()
+		indexService.DiscordBotState.User = &discordgo.User{
+			Username: "test",
+		}
 		err := indexService.DiscordBotState.GuildAdd(&discordgo.Guild{
 			ID: "123",
 			Channels: []*discordgo.Channel{
@@ -60,16 +63,12 @@ func TestNewLinePostDiscordChannelViewHandler(t *testing.T) {
 		assert.NoError(t, err)
 		assert.Len(t, indexService.DiscordBotState.Guilds, 1)
 
-		mux := http.NewServeMux()
-
 		handler := NewIndexViewHandler(indexService)
 
-		mux.HandleFunc("/guilds/{guildId}/line_post_discord_channel", handler.Index)
-
-		req := httptest.NewRequest(http.MethodGet, "/guilds/123/line_post_discord_channel", nil)
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
 		rec := httptest.NewRecorder()
 
-		mux.ServeHTTP(rec, setCtxValue(req))
+		handler.Index(rec, setCtxDiscordUserValue(req))
 
 		assert.Equal(t, http.StatusOK, rec.Code)
 
@@ -77,18 +76,95 @@ func TestNewLinePostDiscordChannelViewHandler(t *testing.T) {
 		assert.Contains(t, rec.Body.String(), `<img src="https://cdn.discordapp.com/avatars/123/test.webp?size=64" alt="Discordアイコン">`)
 		assert.Contains(t, rec.Body.String(), `<p>LINEアカウント: 未ログイン</p>`)
 	})
+
+	t.Run("ルートページが正常に表示される(lineログイン中)", func(t *testing.T) {
+		indexService := &service.IndexService{
+			DiscordSession: &discordgo.Session{},
+		}
+		indexService.DiscordBotState = discordgo.NewState()
+		indexService.DiscordBotState.User = &discordgo.User{
+			Username: "test",
+		}
+		err := indexService.DiscordBotState.GuildAdd(&discordgo.Guild{
+			ID: "123",
+			Channels: []*discordgo.Channel{
+				{
+					ID:       "123",
+					Name:     "test",
+					Position: 1,
+					Type:     discordgo.ChannelTypeGuildText,
+				},
+				{
+					ID:       "1234",
+					Name:     "test",
+					Position: 2,
+					Type:     discordgo.ChannelTypeGuildText,
+				},
+				{
+					ID:       "12345",
+					Name:     "test",
+					Position: 3,
+					Type:     discordgo.ChannelTypeGuildText,
+				},
+			},
+			Members: []*discordgo.Member{
+				{
+					User: &discordgo.User{
+						ID: "123",
+					},
+				},
+			},
+		})
+		assert.NoError(t, err)
+		assert.Len(t, indexService.DiscordBotState.Guilds, 1)
+
+		handler := NewIndexViewHandler(indexService)
+
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		rec := httptest.NewRecorder()
+
+		handler.Index(rec, setCtxLineUserValue(req))
+
+		assert.Equal(t, http.StatusOK, rec.Code)
+
+		assert.Contains(t, rec.Body.String(), `<p>Discordアカウント: 未ログイン</p>`)
+		assert.Contains(t, rec.Body.String(), `<p>LINEアカウント: test</p>`)
+		assert.Contains(t, rec.Body.String(), `<img src="test" style="height:64px;" alt="LINEアイコン">`)
+	})
+
 }
 
-func setCtxValue(r *http.Request) *http.Request {
+func setCtxDiscordUserValue(r *http.Request) *http.Request {
 	ctx := r.Context()
-	ctx = ctxvalue.ContextWithDiscordPermission(ctx, &model.DiscordPermissionData{
-		PermissionCode: 8,
+	ctx = ctxvalue.ContextWithDiscordUser(ctx, &model.DiscordOAuthSession{
+		Token: "test",
 		User: model.DiscordUser{
 			ID:       "123",
 			Username: "test",
 			Avatar:   "test",
 		},
-		Permission: "all",
+	},
+	)
+	return r.WithContext(ctx)
+}
+
+func setCtxLineUserValue(r *http.Request) *http.Request {
+	ctx := r.Context()
+	ctx = ctxvalue.ContextWithLineUser(ctx, &model.LineOAuthSession{
+		Token:          "test",
+		DiscordGuildID: "123",
+		User: model.LineIdTokenUser{
+			Iss:     "https://access.line.me",
+			Sub:     "U123456789abcdef123456789abcdef12",
+			Aud:     "1234567890",
+			Exp:     1504169092,
+			Iat:     1504168492,
+			Nonce:   "0987654321fedcba987654321fedcba9",
+			Amr:     []string{"pwd"},
+			Name:    "test",
+			Picture: "test",
+			Email:   "test",
+		},
 	})
 	return r.WithContext(ctx)
 }

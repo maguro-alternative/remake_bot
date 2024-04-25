550d5dca98c015a3c1a36379acd6a71bf5acffd8
ygo <ka.ra.si.naporitan@gmail.com>
2024-04-25 13:50:14 +0900

    2件と0件のテスト追加

--- web/handler/views/guilds/guilds_test.go
+++ web/handler/views/guilds/guilds_test.go
@@ -115,6 +115,198 @@ func TestNewLinePostDiscordChannelViewHandler(t *testing.T) {
 		assert.Contains(t, rec.Body.String(), `<img src="https://cdn.discordapp.com/icons/123/test.png" alt="test">`)
 		assert.Contains(t, rec.Body.String(), `<li>test</li>`)
 	})
+
+	t.Run("サーバー一覧が正常に表示される(2件)", func(t *testing.T) {
+		indexService := &service.IndexService{
+			Client: mock.NewStubHttpClient(func(req *http.Request) *http.Response {
+				return &http.Response{
+					StatusCode: http.StatusOK,
+					Body: io.NopCloser(strings.NewReader(`
+					[
+						{
+							"id": "123",
+							"name": "test",
+							"icon": "test",
+							"owner": true,
+							"permissions": 1,
+							"features": ["test"]
+						},
+						{
+							"id": "234",
+							"name": "test2",
+							"icon": "test2",
+							"owner": true,
+							"permissions": 1,
+							"features": ["test"]
+						}
+					]
+					`)),
+				}
+			}),
+			DiscordSession: &mock.SessionMock{
+				UserGuildsFunc: func(limit int, beforeID string, afterID string, options ...discordgo.RequestOption) ([]*discordgo.UserGuild, error) {
+					return []*discordgo.UserGuild{
+						{
+							ID:          "123",
+							Name:        "test",
+							Icon:        "test",
+							Owner:       true,
+							Permissions: 1,
+							Features:    []discordgo.GuildFeature{"test"},
+						},
+						{
+							ID:          "234",
+							Name:        "test2",
+							Icon:        "test2",
+							Owner:       true,
+							Permissions: 1,
+							Features:    []discordgo.GuildFeature{"test"},
+						},
+					}, nil
+				},
+			},
+		}
+		indexService.DiscordBotState = discordgo.NewState()
+		err := indexService.DiscordBotState.GuildAdd(&discordgo.Guild{
+			ID: "123",
+			Channels: []*discordgo.Channel{
+				{
+					ID:       "123",
+					Name:     "test",
+					Position: 1,
+					Type:     discordgo.ChannelTypeGuildText,
+				},
+				{
+					ID:       "1234",
+					Name:     "test",
+					Position: 2,
+					Type:     discordgo.ChannelTypeGuildText,
+				},
+				{
+					ID:       "12345",
+					Name:     "test",
+					Position: 3,
+					Type:     discordgo.ChannelTypeGuildText,
+				},
+			},
+			Members: []*discordgo.Member{
+				{
+					User: &discordgo.User{
+						ID: "123",
+					},
+				},
+			},
+		})
+		assert.NoError(t, err)
+		assert.Len(t, indexService.DiscordBotState.Guilds, 1)
+
+		mux := http.NewServeMux()
+
+		handler := NewGuildsViewHandler(indexService)
+
+		mux.HandleFunc("/guilds", handler.Index)
+
+		req := httptest.NewRequest(http.MethodGet, "/guilds", nil)
+		rec := httptest.NewRecorder()
+
+		mux.ServeHTTP(rec, setCtxValue(req))
+
+		assert.Equal(t, http.StatusOK, rec.Code)
+
+		assert.Contains(t, rec.Body.String(), `<p>Discordアカウント: test</p>`)
+		assert.Contains(t, rec.Body.String(), `<img src="https://cdn.discordapp.com/avatars/123/test.webp?size=64" alt="Discordアイコン">`)
+		assert.Contains(t, rec.Body.String(), `<p>LINEアカウント: 未ログイン</p>`)
+
+		assert.Contains(t, rec.Body.String(), `<a href="/guild/123">`)
+		assert.Contains(t, rec.Body.String(), `<img src="https://cdn.discordapp.com/icons/123/test.png" alt="test">`)
+		assert.Contains(t, rec.Body.String(), `<li>test</li>`)
+
+		assert.Contains(t, rec.Body.String(), `<a href="/guild/234">`)
+		assert.Contains(t, rec.Body.String(), `<img src="https://cdn.discordapp.com/icons/234/test2.png" alt="test2">`)
+		assert.Contains(t, rec.Body.String(), `<li>test2</li>`)
+	})
+
+	t.Run("サーバー一覧が正常に表示される(0件)", func(t *testing.T) {
+		indexService := &service.IndexService{
+			Client: mock.NewStubHttpClient(func(req *http.Request) *http.Response {
+				return &http.Response{
+					StatusCode: http.StatusOK,
+					Body: io.NopCloser(strings.NewReader(`
+					[
+						{
+							"id": "123",
+							"name": "test",
+							"icon": "test",
+							"owner": true,
+							"permissions": 1,
+							"features": ["test"]
+						}
+					]
+					`)),
+				}
+			}),
+			DiscordSession: &mock.SessionMock{
+				UserGuildsFunc: func(limit int, beforeID string, afterID string, options ...discordgo.RequestOption) ([]*discordgo.UserGuild, error) {
+					return []*discordgo.UserGuild{}, nil
+				},
+			},
+		}
+		indexService.DiscordBotState = discordgo.NewState()
+		err := indexService.DiscordBotState.GuildAdd(&discordgo.Guild{
+			ID: "123",
+			Channels: []*discordgo.Channel{
+				{
+					ID:       "123",
+					Name:     "test",
+					Position: 1,
+					Type:     discordgo.ChannelTypeGuildText,
+				},
+				{
+					ID:       "1234",
+					Name:     "test",
+					Position: 2,
+					Type:     discordgo.ChannelTypeGuildText,
+				},
+				{
+					ID:       "12345",
+					Name:     "test",
+					Position: 3,
+					Type:     discordgo.ChannelTypeGuildText,
+				},
+			},
+			Members: []*discordgo.Member{
+				{
+					User: &discordgo.User{
+						ID: "123",
+					},
+				},
+			},
+		})
+		assert.NoError(t, err)
+		assert.Len(t, indexService.DiscordBotState.Guilds, 1)
+
+		mux := http.NewServeMux()
+
+		handler := NewGuildsViewHandler(indexService)
+
+		mux.HandleFunc("/guilds", handler.Index)
+
+		req := httptest.NewRequest(http.MethodGet, "/guilds", nil)
+		rec := httptest.NewRecorder()
+
+		mux.ServeHTTP(rec, setCtxValue(req))
+
+		assert.Equal(t, http.StatusOK, rec.Code)
+
+		assert.Contains(t, rec.Body.String(), `<p>Discordアカウント: test</p>`)
+		assert.Contains(t, rec.Body.String(), `<img src="https://cdn.discordapp.com/avatars/123/test.webp?size=64" alt="Discordアイコン">`)
+		assert.Contains(t, rec.Body.String(), `<p>LINEアカウント: 未ログイン</p>`)
+
+		assert.NotContains(t, rec.Body.String(), `<a href="/guild/123">`)
+		assert.NotContains(t, rec.Body.String(), `<img src="https://cdn.discordapp.com/icons/123/test.png" alt="test">`)
+		assert.NotContains(t, rec.Body.String(), `<li>test</li>`)
+	})
+
 }
 
 func setCtxValue(r *http.Request) *http.Request {

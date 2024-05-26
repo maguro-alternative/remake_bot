b975a879833bf08a559d8a574fb2c30f2d749b9f
ygo <ka.ra.si.naporitan@gmail.com>
2024-05-26 12:41:22 +0900

    雛形県政

--- bot/cogs/on_voice_state_update_test.go
+++ bot/cogs/on_voice_state_update_test.go
@@ -9,7 +9,7 @@ import (
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/stretchr/testify/assert"
-	//"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require"
 )
 
 func TestVcSignal(t *testing.T) {
@@ -17,14 +17,70 @@ func TestVcSignal(t *testing.T) {
 	userid := "11"
 	username := "testuser"
 	useravater := "a_"
-	//beforeGuildId := "111"
+	beforeGuildId := "111"
 	afterGuildId := "222"
-	//beforeChannelId := "1111"
+	beforeChannelId := "1111"
 	afterChannelId := "2222"
+	beforeSendChannelId := "11111"
+	afterSendChannelId := "22222"
 
-	// スタブHTTPクライアントを作成
+	discordState := discordgo.NewState()
+	err := discordState.GuildAdd(&discordgo.Guild{
+		ID: afterGuildId,
+		Channels: []*discordgo.Channel{
+			{
+				ID:       afterChannelId,
+				Name:     "after_test_vc",
+				Position: 1,
+				Type:     discordgo.ChannelTypeGuildVoice,
+			},
+			{
+				ID:       afterSendChannelId,
+				Name:     "after_test_text",
+				Position: 2,
+				Type:     discordgo.ChannelTypeGuildText,
+			},
+		},
+		VoiceStates: []*discordgo.VoiceState{
+			{
+				GuildID:   afterGuildId,
+					ChannelID: afterChannelId,
+					Member: &discordgo.Member{
+						User: &discordgo.User{
+							ID:       userid,
+							Username: username,
+							Avatar:   useravater,
+							Bot:      false,
+						},
+					},
+					SelfStream: false,
+					SelfVideo:  false,
+			},
+		},
+	})
+	require.NoError(t, err)
+
+	err = discordState.GuildAdd(&discordgo.Guild{
+		ID: beforeGuildId,
+		Channels: []*discordgo.Channel{
+			{
+				ID:       beforeChannelId,
+				Name:     "before_test_vc",
+				Position: 1,
+				Type:     discordgo.ChannelTypeGuildVoice,
+			},
+			{
+				ID:       beforeSendChannelId,
+				Name:     "vefore_test_text",
+				Position: 2,
+				Type:     discordgo.ChannelTypeGuildText,
+			},
+		},
+	})
+	require.NoError(t, err)
+
 	t.Run("正常系", func(t *testing.T) {
-		_, err := onVoiceStateUpdateFunc(
+		messages, err := onVoiceStateUpdateFunc(
 			ctx,
 			&repository.RepositoryFuncMock{
 				GetVcSignalNgUsersByVcChannelIDAllColumnFunc: func(ctx context.Context, vcChannelID string) ([]*repository.VcSignalNgUserAllColumn, error) {
@@ -34,7 +90,12 @@ func TestVcSignal(t *testing.T) {
 					return []*repository.VcSignalNgRoleAllColumn{}, nil
 				},
 				GetVcSignalChannelAllColumnByVcChannelIDFunc: func(ctx context.Context, vcChannelID string) (*repository.VcSignalChannelAllColumn, error) {
-					return &repository.VcSignalChannelAllColumn{}, nil
+					return &repository.VcSignalChannelAllColumn{
+						VcChannelID:   vcChannelID,
+						GuildID:       afterGuildId,
+						SendSignal:    true,
+						SendChannelID: "123",
+					}, nil
 				},
 				GetVcSignalMentionUsersByVcChannelIDFunc: func(ctx context.Context, vcChannelID string) ([]*repository.VcSignalMentionUser, error) {
 					return []*repository.VcSignalMentionUser{}, nil
@@ -44,31 +105,36 @@ func TestVcSignal(t *testing.T) {
 				},
 			},
 			&mock.SessionMock{
-				ChannelMessageSendFunc :func(channelID string, content string, options ...discordgo.RequestOption) (*discordgo.Message, error) {
-					return &discordgo.Message{}, nil
+				ChannelMessageSendFunc: func(channelID string, content string, options ...discordgo.RequestOption) (*discordgo.Message, error) {
+					return &discordgo.Message{
+						Content: content,
+					}, nil
 				},
-				ChannelMessageSendEmbedFunc :func(channelID string, embed *discordgo.MessageEmbed, options ...discordgo.RequestOption) (*discordgo.Message, error) {
-					return &discordgo.Message{}, nil
+				ChannelMessageSendEmbedFunc: func(channelID string, embed *discordgo.MessageEmbed, options ...discordgo.RequestOption) (*discordgo.Message, error) {
+					return &discordgo.Message{
+						Embeds: []*discordgo.MessageEmbed{embed},
+					}, nil
 				},
 			},
-			&discordgo.State{},
+			discordState,
 			&discordgo.VoiceStateUpdate{
 				VoiceState: &discordgo.VoiceState{
-					GuildID: afterGuildId,
+					GuildID:   afterGuildId,
 					ChannelID: afterChannelId,
 					Member: &discordgo.Member{
 						User: &discordgo.User{
-							ID: userid,
+							ID:       userid,
 							Username: username,
-							Avatar: useravater,
-							Bot: false,
+							Avatar:   useravater,
+							Bot:      false,
 						},
 					},
 					SelfStream: false,
-					SelfVideo: false,
+					SelfVideo:  false,
 				},
 			},
 		)
 		assert.NoError(t, err)
+		assert.Len(t, messages, 2)
 	})
 }

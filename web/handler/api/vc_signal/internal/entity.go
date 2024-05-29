679de7137a3e357bdbcd32cc8121fea21df4b7dd
ygo <ka.ra.si.naporitan@gmail.com>
2024-05-30 07:56:56 +0900

    jsonの型作成

--- web/handler/api/vc_signal/internal/entity.go
+++ web/handler/api/vc_signal/internal/entity.go
@@ -1,13 +1,23 @@
 package internal
 
 import (
-	//validation "github.com/go-ozzo/ozzo-validation"
+	validation "github.com/go-ozzo/ozzo-validation"
 )
 
 type VcSignalJson struct {
-	VcChannelID string `json:"vcChannelId"`
-	SendSignal    bool `json:"sendSignal"`
-	SendChannelId string `json:"sendChannelId"`
-	JoinBot 	 bool `json:"joinBot"`
-	EveryoneMention bool `json:"everyoneMention"`
+	VcChannelID            string   `json:"vcChannelId"`
+	SendSignal             bool     `json:"sendSignal"`
+	SendChannelId          string   `json:"sendChannelId"`
+	JoinBot                bool     `json:"joinBot"`
+	EveryoneMention        bool     `json:"everyoneMention"`
+	VcSignalNgUserIDs      []string `json:"vcSignalNgUserIds"`
+	VcSignalNgRoleIDs      []string `json:"vcSignalNgRoleIds"`
+	VcSignalMentionUserIDs []string `json:"vcSignalMentionUserIds"`
+	VcSignalMentionRoleIDs []string `json:"vcSignalMentionRoleIds"`
+}
+
+func (g VcSignalJson) Validate() error {
+	return validation.ValidateStruct(&g,
+		validation.Field(&g.VcChannelID, validation.Required),
+	)
 }

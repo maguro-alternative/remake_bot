239fc04566909b16a8efccd3bab46d3a8e20ae35
ygo <ka.ra.si.naporitan@gmail.com>
2024-06-02 08:19:50 +0900

    guildid

--- web/handler/api/group/internal/entity.go
+++ web/handler/api/group/internal/entity.go
@@ -6,12 +6,14 @@ import (
 )
 
 type LineBotJson struct {
+	GuildID          string `json:"guildId,omitempty" db:"guild_id"`
 	DefaultChannelID string `json:"defaultChannelId,omitempty" db:"default_channel_id"`
 	DebugMode        bool   `json:"debugMode,omitempty" db:"debug_mode"`
 }
 
 func (g LineBotJson) Validate() error {
 	return validation.ValidateStruct(&g,
+		//validation.Field(&g.GuildID, is.Digit),
 		validation.Field(&g.DefaultChannelID, is.Digit),
 		validation.Field(&g.DefaultChannelID, validation.Required),
 	)

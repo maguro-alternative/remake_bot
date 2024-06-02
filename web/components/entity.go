492e073ae0b6272a9f665fbe45225e483a853f36
ygo <ka.ra.si.naporitan@gmail.com>
2024-06-03 07:59:43 +0900

    不要な構造体の削除

--- web/components/entity.go
+++ web/components/entity.go
@@ -1,15 +1,5 @@
 package components
 
-type DiscordChannelSet struct {
-	ID         string
-	Name       string
-	Ng         bool
-	BotMessage bool
-	NgTypes    []int
-	NgUsers    []string
-	NgRoles    []string
-}
-
 type DiscordChannel struct {
 	ID       string
 	Name     string

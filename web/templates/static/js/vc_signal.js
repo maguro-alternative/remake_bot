d5733943b21772ef7dbd9bd511671cac0ab34fd9
ygo <ka.ra.si.naporitan@gmail.com>
2024-06-04 21:13:59 +0900

    js作成

---
+++ web/templates/static/js/vc_signal.js
@@ -0,0 +1,100 @@
+// submit時にイベント実行をする関数
+document.getElementById('form').onsubmit = async function (event) {
+    let check = window.confirm('送信します。よろしいですか？');
+    if (check === false) {
+        event.preventDefault();
+        return;
+    }
+    // 再読み込み防止
+    event.preventDefault();
+    const guildId = location.pathname.match(/[0-9]/g).join('');
+    const formData = new FormData(document.getElementById('form'));
+    const formElements = document.forms['form'].elements;
+
+    const jsonData = await createJsonData(formElements, formData);
+
+    console.log(jsonData);
+
+    // データを送信
+    await fetch(`/api/${guildId}/vc-signal`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: jsonData
+    }).then((res) => {
+        if (res.ok && res.status === 200) {
+            alert('設定を保存しました');
+            window.location.href = `/guild/${guildId}`;
+        } else {
+            alert('設定の保存に失敗しました');
+        }
+    });
+}
+
+const createJsonData = async function(formElements, formData) {
+    const data = Object.fromEntries(formData.entries());
+    console.log(data);
+    // それぞれ入力内容を配列に格納
+    let ngUserArray, ngRoleArray, mentionUserArray, mentionRoleArray;
+    const channelArray = [];
+    let jsonTmp = {};
+    for (let i = 0; i < formElements.length; i++) {
+        formKey = formElements[i].name;
+        channelIdmatch = formKey.match(/[0-9]/g);
+        if (channelIdmatch === null) {
+            continue;
+        }
+        channelId = channelIdmatch.join('');
+        if (jsonTmp[channelId] === undefined) {
+            jsonTmp[channelId] = {
+                "vcChannelId": channelId,
+                "sendSignal": false,
+                "sendChannelId": formData.get(`defaultChannelId${channelId}`),
+                "joinBot": false,
+                "everyoneMention": false,
+                "vcSignalMentionUsers": mentionUserArray,
+                "vcSignalMentionRoles": mentionRoleArray,
+                "vcSignalNgUsers": ngUserArray,
+                "vcSignalNgRoles": ngRoleArray
+            }
+        }
+        if ((formKey.includes('vcSignalMentionUsers')) && (formKey.includes('[]'))) {
+            jsonTmp[channelId]['vcSignalMentionUsers'] = formData.getAll(formKey);
+        }
+        if ((formKey.includes('vcSignalMentionRoles')) && (formKey.includes('[]'))) {
+            jsonTmp[channelId]['vcSignalMentionRoles'] = formData.getAll(formKey);
+        }
+        if ((formKey.includes('vcSignalNgUsers')) && (formKey.includes('[]'))) {
+            jsonTmp[channelId]['vcSignalNgUsers'] = formData.getAll(formKey);
+        }
+        if ((formKey.includes('vcSignalNgRoles')) && (formKey.includes('[]'))) {
+            jsonTmp[channelId]['vcSignalNgRoles'] = formData.getAll(formKey);
+        }
+        if (formKey.includes('sendSignal')) {
+            document.getElementById(formKey).checked ? jsonTmp[channelId]["sendSignal"] = true : jsonTmp[channelId]["sendSignal"] = false;
+        }
+        if (formKey.includes('defaultChannelId')) {
+            jsonTmp[channelId]["defaultChannelId"] = formData.get(formKey);
+        }
+        if (formKey.includes('joinBot')) {
+            document.getElementById(formKey).checked ? jsonTmp[channelId]["joinBot"] = true : jsonTmp[channelId]["joinBot"] = false;
+        }
+        if (formKey.includes('everyoneMention')) {
+            document.getElementById(formKey).checked ? jsonTmp[channelId]["everyoneMention"] = true : jsonTmp[channelId]["everyoneMention"] = false;
+        }
+    }
+    for (let key in jsonTmp) {
+        channelArray.push(jsonTmp[key]);
+    }
+
+    return JSON.stringify({
+        "vcSignals": channelArray
+    });
+}
+
+try {
+    module.exports = { createJsonData };
+} catch (e) {
+}
+

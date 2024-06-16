ab879b5ab20954fd693e380c974492850865e670
ygo <ka.ra.si.naporitan@gmail.com>
2024-06-17 08:20:18 +0900

    仮のエンティティい

---
+++ web/handler/api/webhook/internal/entity.go
@@ -0,0 +1,21 @@
+package internal
+
+import (
+	validation "github.com/go-ozzo/ozzo-validation"
+)
+
+type WebhookJson struct {
+	Webhooks []Webhook `json:"webhooks"`
+}
+
+type Webhook struct {
+	WebhookID  string `json:"webhookId"`
+	ChannelID  string `json:"channelId"`
+	WebhookURL string `json:"webhookUrl"`
+}
+
+func (g WebhookJson) Validate() error {
+	return validation.ValidateStruct(&g,
+		validation.Field(&g.Webhooks, validation.Required),
+	)
+}

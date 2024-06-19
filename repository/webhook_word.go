49cdbda8353439f252be7e022f297da96ca4b8ef
ygo <ka.ra.si.naporitan@gmail.com>
2024-06-20 08:09:03 +0900

    delete追加

--- repository/webhook_word.go
+++ repository/webhook_word.go
@@ -2,6 +2,8 @@ package repository
 
 import (
 	"context"
+
+	"github.com/maguro-alternative/remake_bot/pkg/db"
 )
 
 type WebhookWord struct {
@@ -50,4 +52,54 @@ func (r *Repository) GetWebhookWordWithWebhookSerialIDAndCondition(
 	var webhookWord []*WebhookWord
 	err := r.db.SelectContext(ctx, &webhookWord, query, webhookSerialID, condition)
 	return webhookWord, err
+}
+
+func (r *Repository) DeleteWebhookWordWithWebhookSerialIDAndCondition(
+	ctx context.Context,
+	webhookSerialID int64,
+	condition string,
+) error {
+	query := `
+		DELETE FROM
+			webhook_word
+		WHERE
+			webhook_serial_id = $1
+			condition = $2
+	`
+	_, err := r.db.ExecContext(ctx, query, webhookSerialID, condition)
+	return err
+}
+
+func (r *Repository) DeleteWebhookWordsNotInProvidedList(
+	ctx context.Context,
+	webhookSerialID int64,
+	conditions string,
+	words []string,
+) error {
+	query := `
+		DELETE FROM
+			webhook_word
+		WHERE
+			webhook_serial_id = ? AND
+			condition = ? AND
+			word NOT IN (?)
+	`
+	if len(words) == 0 {
+		query = `
+			DELETE FROM
+				webhook_word
+			WHERE
+				webhook_serial_id = $1 AND
+				condition = $2
+		`
+		_, err := r.db.ExecContext(ctx, query, webhookSerialID, conditions)
+		return err
+	}
+	query, args, err := db.In(query, webhookSerialID, conditions, words)
+	if err != nil {
+		return err
+	}
+	query = db.Rebind(2, query)
+	_, err = r.db.ExecContext(ctx, query, args...)
+	return err
 }

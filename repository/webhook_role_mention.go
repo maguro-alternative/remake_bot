386df1bb0f0581057f51d41a4db7541df1aadd1f
ygo <ka.ra.si.naporitan@gmail.com>
2024-06-20 17:31:07 +0900

    rolemention追加

---
+++ repository/webhook_role_mention.go
@@ -0,0 +1,83 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/maguro-alternative/remake_bot/pkg/db"
+)
+
+type WebhookRoleMention struct {
+	WebhookSerialID int64  `db:"webhook_serial_id"`
+	RoleID          string `db:"role_id"`
+}
+
+func (r *Repository) InsertWebhookRoleMention(
+	ctx context.Context,
+	webhookSerialID int64,
+	roleID string,
+) error {
+	query := `
+		INSERT INTO webhook_role_mention (
+			webhook_serial_id,
+			role_id
+		) VALUES (
+			$1,
+			$2
+		) ON CONFLICT (webhook_serial_id, role_id) DO NOTHING
+	`
+	_, err := r.db.ExecContext(
+		ctx,
+		query,
+		webhookSerialID,
+		roleID,
+	)
+	return err
+}
+
+func (r *Repository) GetWebhookRoleMentionWithWebhookSerialID(
+	ctx context.Context,
+	webhookSerialID int64,
+) ([]*WebhookRoleMention, error) {
+	query := `
+		SELECT
+			*
+		FROM
+			webhook_role_mention
+		WHERE
+			webhook_serial_id = $1
+	`
+	var webhookRoleMention []*WebhookRoleMention
+	err := r.db.SelectContext(ctx, &webhookRoleMention, query, webhookSerialID)
+	return webhookRoleMention, err
+}
+
+func (r *Repository) DeleteWebhookRoleMentionsNotInProvidedList(
+	ctx context.Context,
+	webhookSerialID int64,
+	roleIDs []string,
+) error {
+	query := `
+		DELETE FROM
+			webhook_role_mention
+		WHERE
+			webhook_serial_id = ? AND
+			role_id NOT IN (?)
+	`
+	if len(roleIDs) == 0 {
+		query = `
+			DELETE FROM
+				webhook_role_mention
+			WHERE
+				webhook_serial_id = $1
+		`
+		_, err := r.db.ExecContext(ctx, query, webhookSerialID)
+		return err
+	}
+	query, args, err := db.In(query, webhookSerialID, roleIDs)
+	if err != nil {
+		return err
+	}
+	query = db.Rebind(2, query)
+	_, err = r.db.ExecContext(ctx, query, args...)
+	return err
+}
---
+++ repository/webhook_role_mention_test.go
@@ -0,0 +1,164 @@
+package repository
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/maguro-alternative/remake_bot/bot/config"
+	"github.com/maguro-alternative/remake_bot/pkg/db"
+	"github.com/maguro-alternative/remake_bot/testutil/fixtures"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWebhookRoleMention(t *testing.T) {
+	ctx := context.Background()
+	lastPostedAt := time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC)
+	t.Run("WebhookRoleMention挿入", func(t *testing.T) {
+		dbV1, cleanup, err := db.NewDB(ctx, config.DatabaseName(), config.DatabaseURLWithSslmode())
+		assert.NoError(t, err)
+		defer cleanup()
+
+		tx, err := dbV1.BeginTxx(ctx, nil)
+		assert.NoError(t, err)
+
+		defer tx.RollbackCtx(ctx)
+
+		tx.ExecContext(ctx, "DELETE FROM webhook_role_mention")
+
+		f := &fixtures.Fixture{DBv1: tx}
+		f.Build(t,
+			fixtures.NewWebhook(ctx, func(b *fixtures.Webhook) {
+				b.GuildID = "1111"
+				b.WebhookID = "22222"
+				b.SubscriptionType = "youtube"
+				b.SubscriptionID = "test"
+				b.LastPostedAt = lastPostedAt
+			}),
+		)
+
+		repo := NewRepository(tx)
+
+		err = repo.InsertWebhookRoleMention(ctx, *f.Webhooks[0].WebhookSerialID, "111111")
+		assert.NoError(t, err)
+
+		var webhookRoleMentions []*WebhookRoleMention
+		err = tx.SelectContext(ctx, &webhookRoleMentions, "SELECT * FROM webhook_role_mention")
+		assert.NoError(t, err)
+		assert.Len(t, webhookRoleMentions, 1)
+	})
+
+	t.Run("WebhookRoleMention挿入(既存のものはスルー)", func(t *testing.T) {
+		dbV1, cleanup, err := db.NewDB(ctx, config.DatabaseName(), config.DatabaseURLWithSslmode())
+		assert.NoError(t, err)
+		defer cleanup()
+
+		tx, err := dbV1.BeginTxx(ctx, nil)
+		assert.NoError(t, err)
+
+		defer tx.RollbackCtx(ctx)
+
+		tx.ExecContext(ctx, "DELETE FROM webhook_role_mention")
+
+		f := &fixtures.Fixture{DBv1: tx}
+		f.Build(t,
+			fixtures.NewWebhook(ctx, func(b *fixtures.Webhook) {
+				b.GuildID = "1111"
+				b.WebhookID = "22222"
+				b.SubscriptionType = "youtube"
+				b.SubscriptionID = "test"
+				b.LastPostedAt = lastPostedAt
+			}),
+		)
+
+		repo := NewRepository(tx)
+
+		err = repo.InsertWebhookRoleMention(ctx, *f.Webhooks[0].WebhookSerialID, "111111")
+		assert.NoError(t, err)
+
+		err = repo.InsertWebhookRoleMention(ctx, *f.Webhooks[0].WebhookSerialID, "111111")
+		assert.NoError(t, err)
+
+		var webhookRoleMentions []*WebhookRoleMention
+		err = tx.SelectContext(ctx, &webhookRoleMentions, "SELECT * FROM webhook_role_mention")
+		assert.NoError(t, err)
+		assert.Len(t, webhookRoleMentions, 1)
+	})
+
+	t.Run("WebhookRoleMention削除", func(t *testing.T) {
+		dbV1, cleanup, err := db.NewDB(ctx, config.DatabaseName(), config.DatabaseURLWithSslmode())
+		assert.NoError(t, err)
+		defer cleanup()
+
+		tx, err := dbV1.BeginTxx(ctx, nil)
+		assert.NoError(t, err)
+
+		defer tx.RollbackCtx(ctx)
+
+		tx.ExecContext(ctx, "DELETE FROM webhook_role_mention")
+
+		f := &fixtures.Fixture{DBv1: tx}
+		f.Build(t,
+			fixtures.NewWebhook(ctx, func(b *fixtures.Webhook) {
+				b.GuildID = "1111"
+				b.WebhookID = "22222"
+				b.SubscriptionType = "youtube"
+				b.SubscriptionID = "test"
+				b.LastPostedAt = lastPostedAt
+			}).Connect(
+				fixtures.NewWebhookRoleMention(ctx, func(b *fixtures.WebhookRoleMention) {
+					b.RoleID = "111111"
+				}),
+			),
+		)
+
+		repo := NewRepository(tx)
+
+		err = repo.DeleteWebhookRoleMentionsNotInProvidedList(ctx, *f.Webhooks[0].WebhookSerialID, []string{})
+		assert.NoError(t, err)
+
+		var webhookRoleMentions []*WebhookRoleMention
+		err = tx.SelectContext(ctx, &webhookRoleMentions, "SELECT * FROM webhook_role_mention")
+		assert.NoError(t, err)
+		assert.Len(t, webhookRoleMentions, 0)
+	})
+
+	t.Run("WebhookRoleMention削除(存在しないものはスルー)", func(t *testing.T) {
+		dbV1, cleanup, err := db.NewDB(ctx, config.DatabaseName(), config.DatabaseURLWithSslmode())
+		assert.NoError(t, err)
+		defer cleanup()
+
+		tx, err := dbV1.BeginTxx(ctx, nil)
+		assert.NoError(t, err)
+
+		defer tx.RollbackCtx(ctx)
+
+		tx.ExecContext(ctx, "DELETE FROM webhook_role_mention")
+
+		f := &fixtures.Fixture{DBv1: tx}
+		f.Build(t,
+			fixtures.NewWebhook(ctx, func(b *fixtures.Webhook) {
+				b.GuildID = "1111"
+				b.WebhookID = "22222"
+				b.SubscriptionType = "youtube"
+				b.SubscriptionID = "test"
+				b.LastPostedAt = lastPostedAt
+			}).Connect(
+				fixtures.NewWebhookRoleMention(ctx, func(b *fixtures.WebhookRoleMention) {
+					b.RoleID = "111111"
+				}),
+			),
+		)
+
+		repo := NewRepository(tx)
+
+		err = repo.DeleteWebhookRoleMentionsNotInProvidedList(ctx, *f.Webhooks[0].WebhookSerialID, []string{"111111", "222222"})
+		assert.NoError(t, err)
+
+		var webhookRoleMentions []*WebhookRoleMention
+		err = tx.SelectContext(ctx, &webhookRoleMentions, "SELECT * FROM webhook_role_mention")
+		assert.NoError(t, err)
+		assert.Len(t, webhookRoleMentions, 1)
+	})
+}

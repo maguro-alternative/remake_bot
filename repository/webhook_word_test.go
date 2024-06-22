826906f8f53dbdd76e90c18671efcdde13acfd22
ygo <ka.ra.si.naporitan@gmail.com>
2024-06-22 09:39:16 +0900

    テスト追加

--- repository/webhook_word_test.go
+++ repository/webhook_word_test.go
@@ -167,4 +167,165 @@ func TestWebhookWord(t *testing.T) {
 		assert.NoError(t, err)
 		assert.Len(t, webhookWords, 1)
 	})
+
+	t.Run("WebhookWord削除(ng_or以外は削除しない)", func(t *testing.T) {
+		dbV1, cleanup, err := db.NewDB(ctx, config.DatabaseName(), config.DatabaseURLWithSslmode())
+		assert.NoError(t, err)
+		defer cleanup()
+
+		tx, err := dbV1.BeginTxx(ctx, nil)
+		assert.NoError(t, err)
+
+		defer tx.RollbackCtx(ctx)
+
+		tx.ExecContext(ctx, "DELETE FROM webhook_word")
+
+		repo := NewRepository(tx)
+
+		f := &fixtures.Fixture{DBv1: tx}
+		f.Build(t,
+			fixtures.NewWebhook(ctx, func(b *fixtures.Webhook) {
+				b.GuildID = "1111"
+				b.WebhookID = "22222"
+				b.SubscriptionType = "youtube"
+				b.SubscriptionID = "test"
+				b.LastPostedAt = lastPostedAt
+			}).Connect(
+				fixtures.NewWebhookWord(ctx, func(b *fixtures.WebhookWord) {
+					b.Condition = "ng_or"
+					b.Word = "word"
+				}),
+				fixtures.NewWebhookWord(ctx, func(b *fixtures.WebhookWord) {
+					b.Condition = "ng_and"
+					b.Word = "word2"
+				}),
+			),
+		)
+
+		err = repo.DeleteWebhookWordsNotInProvidedList(ctx, *f.Webhooks[0].WebhookSerialID, "ng_or", []string{"word"})
+		assert.NoError(t, err)
+
+		var webhookWords []*WebhookWord
+		err = tx.SelectContext(ctx, &webhookWords, "SELECT * FROM webhook_word")
+		assert.NoError(t, err)
+		assert.Len(t, webhookWords, 2)
+	})
+
+	t.Run("WebhookWord削除(削除はしない)", func(t *testing.T) {
+		dbV1, cleanup, err := db.NewDB(ctx, config.DatabaseName(), config.DatabaseURLWithSslmode())
+		assert.NoError(t, err)
+		defer cleanup()
+
+		tx, err := dbV1.BeginTxx(ctx, nil)
+		assert.NoError(t, err)
+
+		defer tx.RollbackCtx(ctx)
+
+		tx.ExecContext(ctx, "DELETE FROM webhook_word")
+
+		repo := NewRepository(tx)
+
+		f := &fixtures.Fixture{DBv1: tx}
+		f.Build(t,
+			fixtures.NewWebhook(ctx, func(b *fixtures.Webhook) {
+				b.GuildID = "1111"
+				b.WebhookID = "22222"
+				b.SubscriptionType = "youtube"
+				b.SubscriptionID = "test"
+				b.LastPostedAt = lastPostedAt
+			}).Connect(
+				fixtures.NewWebhookWord(ctx, func(b *fixtures.WebhookWord) {
+					b.Condition = "ng_or"
+					b.Word = "word"
+				}),
+				fixtures.NewWebhookWord(ctx, func(b *fixtures.WebhookWord) {
+					b.Condition = "ng_and"
+					b.Word = "word2"
+				}),
+			),
+			fixtures.NewWebhook(ctx, func(b *fixtures.Webhook) {
+				b.GuildID = "1111"
+				b.WebhookID = "22223"
+				b.SubscriptionType = "youtube"
+				b.SubscriptionID = "test"
+				b.LastPostedAt = lastPostedAt
+			}).Connect(
+				fixtures.NewWebhookWord(ctx, func(b *fixtures.WebhookWord) {
+					b.Condition = "ng_or"
+					b.Word = "word"
+				}),
+				fixtures.NewWebhookWord(ctx, func(b *fixtures.WebhookWord) {
+					b.Condition = "ng_and"
+					b.Word = "word2"
+				}),
+			),
+		)
+
+		err = repo.DeleteWebhookWordsNotInProvidedList(ctx, *f.Webhooks[0].WebhookSerialID, "ng_or", []string{"word"})
+		assert.NoError(t, err)
+
+		var webhookWords []*WebhookWord
+		err = tx.SelectContext(ctx, &webhookWords, "SELECT * FROM webhook_word")
+		assert.NoError(t, err)
+		assert.Len(t, webhookWords, 2)
+	})
+
+	t.Run("WebhookWord削除(はじめのword2を削除)", func(t *testing.T) {
+		dbV1, cleanup, err := db.NewDB(ctx, config.DatabaseName(), config.DatabaseURLWithSslmode())
+		assert.NoError(t, err)
+		defer cleanup()
+
+		tx, err := dbV1.BeginTxx(ctx, nil)
+		assert.NoError(t, err)
+
+		defer tx.RollbackCtx(ctx)
+
+		tx.ExecContext(ctx, "DELETE FROM webhook_word")
+
+		repo := NewRepository(tx)
+
+		f := &fixtures.Fixture{DBv1: tx}
+		f.Build(t,
+			fixtures.NewWebhook(ctx, func(b *fixtures.Webhook) {
+				b.GuildID = "1111"
+				b.WebhookID = "22222"
+				b.SubscriptionType = "youtube"
+				b.SubscriptionID = "test"
+				b.LastPostedAt = lastPostedAt
+			}).Connect(
+				fixtures.NewWebhookWord(ctx, func(b *fixtures.WebhookWord) {
+					b.Condition = "ng_or"
+					b.Word = "word"
+				}),
+				fixtures.NewWebhookWord(ctx, func(b *fixtures.WebhookWord) {
+					b.Condition = "ng_or"
+					b.Word = "word2"
+				}),
+			),
+			fixtures.NewWebhook(ctx, func(b *fixtures.Webhook) {
+				b.GuildID = "1111"
+				b.WebhookID = "22223"
+				b.SubscriptionType = "youtube"
+				b.SubscriptionID = "test"
+				b.LastPostedAt = lastPostedAt
+			}).Connect(
+				fixtures.NewWebhookWord(ctx, func(b *fixtures.WebhookWord) {
+					b.Condition = "ng_or"
+					b.Word = "word"
+				}),
+				fixtures.NewWebhookWord(ctx, func(b *fixtures.WebhookWord) {
+					b.Condition = "ng_and"
+					b.Word = "word2"
+				}),
+			),
+		)
+
+		err = repo.DeleteWebhookWordsNotInProvidedList(ctx, *f.Webhooks[0].WebhookSerialID, "ng_or", []string{"word"})
+		assert.NoError(t, err)
+
+		var webhookWords []*WebhookWord
+		err = tx.SelectContext(ctx, &webhookWords, "SELECT * FROM webhook_word")
+		assert.NoError(t, err)
+		assert.Len(t, webhookWords, 3)
+	})
 }

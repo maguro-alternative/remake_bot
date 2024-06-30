6b70b2cab5fec54d76d1310bcaef6760269d4818
ygo <ka.ra.si.naporitan@gmail.com>
2024-06-30 14:35:21 +0900

    viewのテスト追加

--- web/handler/views/guildid/webhook/webhook_test.go
+++ web/handler/views/guildid/webhook/webhook_test.go
@@ -26,7 +26,7 @@ func TestNewWebhookViewHandler(t *testing.T) {
 		require.NoError(t, os.Chdir(cwd))
 	})
 	require.NoError(t, os.Chdir("../../../../../"))
-	t.Run("正常に表示される", func(t *testing.T) {
+	t.Run("正常に表示される(データベースは空)", func(t *testing.T) {
 		indexService := &service.IndexService{
 			DiscordSession: &mock.SessionMock{
 				GuildWebhooksFunc: func(guildID string, options ...discordgo.RequestOption) (st []*discordgo.Webhook, err error) {
@@ -119,6 +119,128 @@ func TestNewWebhookViewHandler(t *testing.T) {
 		assert.Contains(t, rec.Body.String(), `<button type="button" onclick="addWord('newMentionOr', 1)">メンションOR検索追加</button>`)
 		assert.Contains(t, rec.Body.String(), `<div id="newMentionAndWords1">`)
 		assert.Contains(t, rec.Body.String(), `<button type="button" onclick="addWord('newMentionAnd', 1)">メンションAND検索追加</button>`)
+		assert.Contains(t, rec.Body.String(), `<button type="submit" class="btn btn-primary">送信</button>`)
+	})
+
+	t.Run("正常に表示される(データベースにデータがある)", func(t *testing.T) {
+		serialId := int64(1)
+		indexService := &service.IndexService{
+			DiscordSession: &mock.SessionMock{
+				GuildWebhooksFunc: func(guildID string, options ...discordgo.RequestOption) (st []*discordgo.Webhook, err error) {
+					st = []*discordgo.Webhook{
+						{
+							ID:        "11",
+							Type:      1,
+							GuildID:   "123",
+							ChannelID: "234",
+							Name:      "test",
+						},
+					}
+					return st, err
+				},
+			},
+		}
+		indexService.DiscordBotState = discordgo.NewState()
+		err := indexService.DiscordBotState.GuildAdd(&discordgo.Guild{
+			ID: "123",
+			Members: []*discordgo.Member{
+				{
+					User: &discordgo.User{
+						ID:       "1234",
+						Username: "test",
+					},
+				},
+			},
+			Roles: []*discordgo.Role{
+				{
+					ID:   "1235",
+					Name: "test",
+				},
+			},
+		})
+		assert.NoError(t, err)
+
+		mux := http.NewServeMux()
+
+		repo := &repository.RepositoryFuncMock{
+			GetAllColumnsWebhooksByGuildIDFunc: func(ctx context.Context, guildID string) ([]*repository.Webhook, error) {
+				return []*repository.Webhook{
+					{
+						WebhookSerialID: &serialId,
+						GuildID:         "123",
+						WebhookID:       "11",
+						SubscriptionType: "youtube",
+						SubscriptionID:   "test",
+					},
+				}, nil
+			},
+			GetWebhookWordWithWebhookSerialIDAndConditionFunc: func(ctx context.Context, webhookSerialID int64, condition string) ([]*repository.WebhookWord, error) {
+				return []*repository.WebhookWord{
+					{
+						WebhookSerialID: 1,
+						Condition:       "NgOr",
+						Word:            "test",
+					},
+				}, nil
+			},
+			GetWebhookUserMentionWithWebhookSerialIDFunc: func(ctx context.Context, webhookSerialID int64) ([]*repository.WebhookUserMention, error) {
+				return []*repository.WebhookUserMention{
+					{
+						WebhookSerialID: 1,
+						UserID:          "1234",
+					},
+				}, nil
+			},
+			GetWebhookRoleMentionWithWebhookSerialIDFunc: func(ctx context.Context, webhookSerialID int64) ([]*repository.WebhookRoleMention, error) {
+				return []*repository.WebhookRoleMention{
+					{
+						WebhookSerialID: 1,
+						RoleID:          "1235",
+					},
+				}, nil
+			},
+		}
+
+		handler := NewWebhookViewHandler(indexService, repo)
+
+		mux.HandleFunc("/guild/{guildId}/webhook", handler.Index)
+
+		req := httptest.NewRequest(http.MethodGet, "/guild/123/webhook", nil)
+		rec := httptest.NewRecorder()
+
+		mux.ServeHTTP(rec, setCtxValue(req))
+
+		assert.Equal(t, http.StatusOK, rec.Code)
+
+		assert.Contains(t, rec.Body.String(), `<button type="button" onclick="addWebhook()">追加</button>`)
+		assert.Contains(t, rec.Body.String(), `<label for="updateWebhookType1">Webhook</label>`)
+		assert.Contains(t, rec.Body.String(), `<select name="updateWebhookType1" id="updateWebhookType1">`)
+		assert.Contains(t, rec.Body.String(), `<option value="11" selected>test</option>`)
+		assert.Contains(t, rec.Body.String(), `<label for="updateSubscriptionName1">サービス名</label>`)
+		assert.Contains(t, rec.Body.String(), `<select name="updateSubscriptionName1" id="updateSubscriptionName1" />`)
+		assert.Contains(t, rec.Body.String(), `<option value="youtube" selected>youtube</option>`)
+		assert.Contains(t, rec.Body.String(), `<option value="niconico">niconico</option>`)
+		assert.Contains(t, rec.Body.String(), `<label for="updateSubscriptionId1">サービスID</label>`)
+		assert.Contains(t, rec.Body.String(), `<input type="text" name="updateSubscriptionId1" id="updateSubscriptionId1" value="test" />`)
+		assert.Contains(t, rec.Body.String(), `<label for="updateMemberMention1[]">メンションするユーザー</label>`)
+		assert.Contains(t, rec.Body.String(), `<select name="updateMemberMention1[]" id="updateMemberMention1[]" multiple>`)
+		assert.Contains(t, rec.Body.String(), `<option value="1234" selected>test</option>`)
+		assert.Contains(t, rec.Body.String(), `<label for="updateRoleMention1[]">メンションするロール</label>`)
+		assert.Contains(t, rec.Body.String(), `<select name="updateRoleMention1[]" id="updateRoleMention1[]" multiple>`)
+		assert.Contains(t, rec.Body.String(), `<option value="1235" selected>test</option>`)
+		assert.Contains(t, rec.Body.String(), `<div id="updateNgOrWords1">`)
+		assert.Contains(t, rec.Body.String(), `<button type="button" onclick="addWord('updateNgOr', 1)">NGワードOR検索追加</button>`)
+		assert.Contains(t, rec.Body.String(), `<input type="text" id="updateNgOrWord1[]" name="updateNgOrWord1[]" value="test">`)
+		assert.Contains(t, rec.Body.String(), `<div id="updateNgAndWords1">`)
+		assert.Contains(t, rec.Body.String(), `<button type="button" onclick="addWord('updateNgAnd', 1)">NGワードAND検索追加</button>`)
+		assert.Contains(t, rec.Body.String(), `<div id="updateSearchOrWords1">`)
+		assert.Contains(t, rec.Body.String(), `<button type="button" onclick="addWord('updateSearchOr', 1)">キーワードOR検索追加</button>`)
+		assert.Contains(t, rec.Body.String(), `<div id="updateSearchAndWords1">`)
+		assert.Contains(t, rec.Body.String(), `<button type="button" onclick="addWord('updateSearchAnd', 1)">キーワードAND検索追加</button>`)
+		assert.Contains(t, rec.Body.String(), `<div id="updateMentionOrWords1">`)
+		assert.Contains(t, rec.Body.String(), `<button type="button" onclick="addWord('updateMentionOr', 1)">メンションOR検索追加</button>`)
+		assert.Contains(t, rec.Body.String(), `<div id="updateMentionAndWords1">`)
+		assert.Contains(t, rec.Body.String(), `<button type="button" onclick="addWord('updateMentionAnd', 1)">メンションAND検索追加</button>`)
 		assert.Contains(t, rec.Body.String(), `<button type="submit" class="btn btn-primary">送信</button>`)
 	})
 }

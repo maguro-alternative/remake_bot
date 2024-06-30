ea9bc1060748ff0a7d877ae9524e014cce80b667
ygo <ka.ra.si.naporitan@gmail.com>
2024-06-30 16:49:52 +0900

    GetWebhookUserMentionWithWebhookSerialIDs削除

--- repository/webhook_user_mention_test.go
+++ repository/webhook_user_mention_test.go
@@ -122,53 +122,6 @@ func TestWebhookUserMention(t *testing.T) {
 		assert.Equal(t, "111111", webhookUserMentions[0].UserID)
 	})
 
-	t.Run("WebhookUserMention取得(idsから取得)", func(t *testing.T) {
-		dbV1, cleanup, err := db.NewDB(ctx, config.DatabaseName(), config.DatabaseURLWithSslmode())
-		assert.NoError(t, err)
-		defer cleanup()
-
-		tx, err := dbV1.BeginTxx(ctx, nil)
-		assert.NoError(t, err)
-
-		defer tx.RollbackCtx(ctx)
-
-		tx.ExecContext(ctx, "DELETE FROM webhook_user_mention")
-
-		f := &fixtures.Fixture{DBv1: tx}
-		f.Build(t,
-			fixtures.NewWebhook(ctx, func(b *fixtures.Webhook) {
-				b.GuildID = "1111"
-				b.WebhookID = "22222"
-				b.SubscriptionType = "youtube"
-				b.SubscriptionID = "test"
-				b.LastPostedAt = lastPostedAt
-			}).Connect(
-				fixtures.NewWebhookUserMention(ctx, func(b *fixtures.WebhookUserMention) {
-					b.UserID = "111111"
-				}),
-			),
-			fixtures.NewWebhook(ctx, func(b *fixtures.Webhook) {
-				b.GuildID = "1111"
-				b.WebhookID = "22223"
-				b.SubscriptionType = "youtube"
-				b.SubscriptionID = "test"
-				b.LastPostedAt = lastPostedAt
-			}).Connect(
-				fixtures.NewWebhookUserMention(ctx, func(b *fixtures.WebhookUserMention) {
-					b.UserID = "111111"
-				}),
-			),
-		)
-
-		repo := NewRepository(tx)
-
-		webhookUserMentions, err := repo.GetWebhookUserMentionWithWebhookSerialIDs(ctx, []int64{*f.Webhooks[0].WebhookSerialID, *f.Webhooks[1].WebhookSerialID})
-		assert.NoError(t, err)
-		assert.Len(t, webhookUserMentions, 2)
-		assert.Equal(t, "111111", webhookUserMentions[0].UserID)
-		assert.Equal(t, "111111", webhookUserMentions[1].UserID)
-	})
-
 	t.Run("WebhookUserMention削除(指定したもの以外は削除される)", func(t *testing.T) {
 		dbV1, cleanup, err := db.NewDB(ctx, config.DatabaseName(), config.DatabaseURLWithSslmode())
 		assert.NoError(t, err)

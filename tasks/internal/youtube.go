35ccace12e01451148934a3f5d42a60c8a570f48
ygo <ka.ra.si.naporitan@gmail.com>
2024-07-01 14:11:32 +0900

    repo追加

--- tasks/internal/youtube.go
+++ tasks/internal/youtube.go
@@ -3,6 +3,7 @@ package internal
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/maguro-alternative/remake_bot/repository"
 
@@ -12,10 +13,13 @@ import (
 
 func Run(
 	ctx context.Context,
+	repo repository.Repository,
 	webhook repository.Webhook,
 	discordSession *discordgo.Session,
 ) ([]*discordgo.Message, error) {
 	var messages []*discordgo.Message
+	var mentionsMessage string
+	var lastPostedAt time.Time
 	// ここにタスクを書く
 	feed, err := gofeed.NewParser().ParseURL(fmt.Sprintf("https://www.youtube.com/feeds/videos.xml?channel_id=%s", webhook.SubscriptionID))
     if err != nil {
@@ -30,13 +34,36 @@ func Run(
 			if err != nil {
 				return nil, err
 			}
+			userMentions, err := repo.GetWebhookUserMentionWithWebhookSerialID(ctx, *webhook.WebhookSerialID)
+			if err != nil {
+				return nil, err
+			}
+			roleMentions, err := repo.GetWebhookRoleMentionWithWebhookSerialID(ctx, *webhook.WebhookSerialID)
+			if err != nil {
+				return nil, err
+			}
+			for _, userMention := range userMentions {
+				mentionsMessage += fmt.Sprintf("<@%s> ", userMention.UserID)
+			}
+			for _, roleMention := range roleMentions {
+				mentionsMessage += fmt.Sprintf("<@&%s> ", roleMention.RoleID)
+			}
 			message, err := discordSession.WebhookExecute(w.ID, w.Token, false, &discordgo.WebhookParams{
-				Content: fmt.Sprintf("%s\n%s", item.Title, item.Link),
+				Content: fmt.Sprintf("%s\n%s\n%s", mentionsMessage, item.Title, item.Link),
 			})
 			if err != nil {
 				return nil, err
 			}
+			if lastPostedAt.Before(*item.PublishedParsed) {
+				lastPostedAt = *item.PublishedParsed
+			}
 			messages = append(messages, message)
+		}
+	}
+	if !lastPostedAt.IsZero() {
+		err = repo.UpdateWebhookWithLastPostedAt(ctx, *webhook.WebhookSerialID, lastPostedAt)
+		if err != nil {
+			return nil, err
 		}
 	}
 	return messages, nil

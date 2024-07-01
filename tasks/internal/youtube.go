3547eb058daf0b88c0d86d57c87f1a6adc19daae
ygo <ka.ra.si.naporitan@gmail.com>
2024-07-01 09:35:48 +0900

    仮組

--- tasks/internal/youtube.go
+++ tasks/internal/youtube.go
@@ -4,25 +4,40 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/bwmarrin/discordgo"
+	"github.com/maguro-alternative/remake_bot/repository"
 
+	"github.com/bwmarrin/discordgo"
 	"github.com/mmcdole/gofeed"
 )
 
 func Run(
 	ctx context.Context,
-	youtubeId string,
-	discord *discordgo.Session,
-) error {
+	webhook repository.Webhook,
+	discordSession *discordgo.Session,
+) ([]*discordgo.Message, error) {
+	var messages []*discordgo.Message
 	// ここにタスクを書く
-	feed, err := gofeed.NewParser().ParseURL(fmt.Sprintf("https://www.youtube.com/feeds/videos.xml?channel_id=%s", youtubeId))
+	feed, err := gofeed.NewParser().ParseURL(fmt.Sprintf("https://www.youtube.com/feeds/videos.xml?channel_id=%s", webhook.SubscriptionID))
     if err != nil {
-		return err
+		return nil, err
 	}
 	for _, item := range feed.Items {
         if item == nil {
             break
         }
+		if webhook.LastPostedAt.After(*item.PublishedParsed) {
+			w, err := discordSession.Webhook(webhook.WebhookID)
+			if err != nil {
+				return nil, err
+			}
+			message, err := discordSession.WebhookExecute(w.ID, w.Token, false, &discordgo.WebhookParams{
+				Content: fmt.Sprintf("%s\n%s", item.Title, item.Link),
+			})
+			if err != nil {
+				return nil, err
+			}
+			messages = append(messages, message)
+		}
 	}
-	return nil
+	return messages, nil
 }

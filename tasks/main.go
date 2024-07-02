da6503135133e8c24da80a48fa1a4811966f63d0
ygo <ka.ra.si.naporitan@gmail.com>
2024-07-02 09:44:25 +0900

    repo追加

--- tasks/main.go
+++ tasks/main.go
@@ -2,23 +2,40 @@ package tasks
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"github.com/maguro-alternative/remake_bot/pkg/db"
+
+	"github.com/maguro-alternative/remake_bot/repository"
+
+	"github.com/bwmarrin/discordgo"
 )
 
-func Run(ctx context.Context, dbv1 db.Driver,) error {
+func Run(ctx context.Context, dbv1 db.Driver, discord *discordgo.Session) error {
 	// ここにタスクを書く
 	oneMinute := time.NewTicker(1 * time.Minute)
 	tenMinute := time.NewTicker(10 * time.Minute)
+	repo := repository.NewRepository(dbv1)
+	webhooks, err := repo.GetAllColumnsWebhooks(ctx)
+	if err != nil {
+		return err
+	}
 	for {
 		select {
 		case <-oneMinute.C:
-			fmt.Println("1分経過")
+			for _, webhook := range webhooks {
+				switch webhook.SubscriptionType {
+				case "youtube":
+					// ここにタスクを書く
+				case "niconico":
+					// ここにタスクを書く
+				}
+			}
 		case <-tenMinute.C:
-			fmt.Println("10分経過")
+			webhooks, err = repo.GetAllColumnsWebhooks(ctx)
+			if err != nil {
+				return err
+			}
 		}
 	}
-	return nil
 }

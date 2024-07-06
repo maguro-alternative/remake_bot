4c9aa36c16643aaaa3109e7a242ce8f2bf676f01
ygo <ka.ra.si.naporitan@gmail.com>
2024-07-06 10:52:43 +0900

    clientをスタブ化

--- bot/commands/voicevox_test.go
+++ bot/commands/voicevox_test.go
@@ -3,6 +3,8 @@ package commands
 import (
 	"net/http"
 	"testing"
+	"io"
+	"strings"
 
 	"github.com/maguro-alternative/remake_bot/repository"
 	"github.com/maguro-alternative/remake_bot/testutil/mock"
@@ -15,9 +17,14 @@ func Test_VoiceVoxCommand(t *testing.T) {
 	repo := &repository.RepositoryFuncMock{}
 	session := mock.SessionMock{}
 	state := discordgo.NewState()
-	client := &http.Client{}
+	stubClient := mock.NewStubHttpClient(func(req *http.Request) *http.Response {
+		return &http.Response{
+			StatusCode: http.StatusOK,
+			Body:       io.NopCloser(strings.NewReader("")),
+		}
+	})
 	t.Run("voicevox成功", func(t *testing.T) {
-		err := VoiceVoxCommand(repo, client).Executor(&session, state, nil, &discordgo.InteractionCreate{
+		err := VoiceVoxCommand(repo, stubClient).Executor(&session, state, nil, &discordgo.InteractionCreate{
 			Interaction: &discordgo.Interaction{
 				Data: discordgo.ApplicationCommandInteractionData{
 					Name: "voicevox",

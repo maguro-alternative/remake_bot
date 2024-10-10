7e1eeca32ab4ace33251feb224c193bbd8e461d7
maguro-alternative <ka.ra.si.naporitan@gmail.com>
2024-10-10 09:25:54 +0900

    niconico追加

---
+++ tasks/internal/niconico_test.go
@@ -0,0 +1,164 @@
+package internal
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/maguro-alternative/remake_bot/repository"
+	"github.com/maguro-alternative/remake_bot/testutil/mock"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/mmcdole/gofeed"
+)
+
+func TestNiconicoRssReader(t *testing.T) {
+	ctx := context.Background()
+	previousPostAt := time.Date(2020, 12, 31, 0, 0, 0, 0, time.UTC)
+	beforePostAt := time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC)
+	afterPostAt := time.Date(2021, 1, 2, 0, 0, 0, 0, time.UTC)
+	discordSession := &mock.SessionMock{
+		WebhookFunc: func(webhookID string, options ...discordgo.RequestOption) (*discordgo.Webhook, error) {
+			return &discordgo.Webhook{}, nil
+		},
+		WebhookExecuteFunc: func(webhookID string, token string, wait bool, data *discordgo.WebhookParams, options ...discordgo.RequestOption) (*discordgo.Message, error) {
+			return &discordgo.Message{
+				Content: data.Content,
+			}, nil
+		},
+		WebhookThreadExecuteFunc: func(webhookID, token string, wait bool, threadID string, data *discordgo.WebhookParams, options ...discordgo.RequestOption) (st *discordgo.Message, err error) {
+			return &discordgo.Message{
+				ChannelID: threadID,
+				Content:   data.Content,
+			}, nil
+		},
+	}
+	repo := &repository.RepositoryFuncMock{
+		GetWebhookUserMentionWithWebhookSerialIDFunc: func(ctx context.Context, webhookSerialID int64) ([]*repository.WebhookUserMention, error) {
+			return []*repository.WebhookUserMention{}, nil
+		},
+		GetWebhookRoleMentionWithWebhookSerialIDFunc: func(ctx context.Context, webhookSerialID int64) ([]*repository.WebhookRoleMention, error) {
+			return []*repository.WebhookRoleMention{}, nil
+		},
+		UpdateWebhookWithLastPostedAtFunc: func(ctx context.Context, webhookSerialID int64, lastPostedAt time.Time) error {
+			return nil
+		},
+		GetWebhookThreadWithWebhookSerialIDFunc: func(ctx context.Context, webhookSerialID int64) ([]*repository.WebhookThread, error) {
+			return []*repository.WebhookThread{}, nil
+		},
+	}
+	webhookSerialId := int64(1)
+	webhook := repository.Webhook{
+		WebhookSerialID:  &webhookSerialId,
+		GuildID:          "1111",
+		WebhookID:        "2222",
+		SubscriptionType: "niconico",
+		SubscriptionID:   "test",
+		LastPostedAt:     beforePostAt,
+	}
+	feed := &gofeed.Feed{
+		Items: []*gofeed.Item{
+			{
+				Title:           "test",
+				Link:            "https://www.niconico.jp/smXXXX?ref=rss_myvideo_rss2.0",
+				PublishedParsed: &afterPostAt,
+			},
+			{
+				Title:           "test2",
+				Link:            "https://www.niconico.jp/smXXXXX?ref=rss_myvideo_rss2.0",
+				PublishedParsed: &previousPostAt,
+			},
+		},
+	}
+	t.Run("niconicoのRss取得に成功すること", func(t *testing.T) {
+		messages, err := run(ctx, discordSession, repo, webhook, feed)
+		assert.NoError(t, err)
+		assert.Len(t, messages, 1)
+		assert.Equal(t, "test\nhttps://www.niconico.jp/smXXXX?ref=rss_myvideo_rss2.0", messages[0].Content)
+	})
+
+	t.Run("niconicoのRss取得に失敗すること", func(t *testing.T) {
+		discordSession := &mock.SessionMock{
+			WebhookFunc: func(webhookID string, options ...discordgo.RequestOption) (*discordgo.Webhook, error) {
+				return nil, assert.AnError
+			},
+		}
+		_, err := run(ctx, discordSession, repo, webhook, feed)
+		assert.Error(t, err)
+	})
+
+	t.Run("ユーザメンションを含めること", func(t *testing.T) {
+		repo.GetWebhookUserMentionWithWebhookSerialIDFunc = func(ctx context.Context, webhookSerialID int64) ([]*repository.WebhookUserMention, error) {
+			return []*repository.WebhookUserMention{
+				{
+					WebhookSerialID: webhookSerialId,
+					UserID:          "3333",
+				},
+			}, nil
+		}
+		messages, err := run(ctx, discordSession, repo, webhook, feed)
+		assert.NoError(t, err)
+		assert.Len(t, messages, 1)
+		assert.Equal(t, "<@3333> \ntest\nhttps://www.niconico.jp/smXXXX?ref=rss_myvideo_rss2.0", messages[0].Content)
+	})
+
+	t.Run("ロールメンションを含めること", func(t *testing.T) {
+		repo.GetWebhookUserMentionWithWebhookSerialIDFunc = func(ctx context.Context, webhookSerialID int64) ([]*repository.WebhookUserMention, error) {
+			return []*repository.WebhookUserMention{}, nil
+		}
+		repo.GetWebhookRoleMentionWithWebhookSerialIDFunc = func(ctx context.Context, webhookSerialID int64) ([]*repository.WebhookRoleMention, error) {
+			return []*repository.WebhookRoleMention{
+				{
+					WebhookSerialID: webhookSerialId,
+					RoleID:          "4444",
+				},
+			}, nil
+		}
+		messages, err := run(ctx, discordSession, repo, webhook, feed)
+		assert.NoError(t, err)
+		assert.Len(t, messages, 1)
+		assert.Equal(t, "<@&4444> \ntest\nhttps://www.niconico.jp/smXXXX?ref=rss_myvideo_rss2.0", messages[0].Content)
+	})
+
+	t.Run("Threadに投稿すること", func(t *testing.T) {
+		repo := &repository.RepositoryFuncMock{
+			GetWebhookUserMentionWithWebhookSerialIDFunc: func(ctx context.Context, webhookSerialID int64) ([]*repository.WebhookUserMention, error) {
+				return []*repository.WebhookUserMention{}, nil
+			},
+			GetWebhookRoleMentionWithWebhookSerialIDFunc: func(ctx context.Context, webhookSerialID int64) ([]*repository.WebhookRoleMention, error) {
+				return []*repository.WebhookRoleMention{}, nil
+			},
+			UpdateWebhookWithLastPostedAtFunc: func(ctx context.Context, webhookSerialID int64, lastPostedAt time.Time) error {
+				return nil
+			},
+			GetWebhookThreadWithWebhookSerialIDFunc: func(ctx context.Context, webhookSerialID int64) ([]*repository.WebhookThread, error) {
+				return []*repository.WebhookThread{
+					{
+						WebhookSerialID: webhookSerialID,
+						ThreadID:        "111",
+					},
+				}, nil
+			},
+		}
+		messages, err := run(ctx, discordSession, repo, webhook, feed)
+		assert.NoError(t, err)
+		assert.Len(t, messages, 1)
+		assert.Equal(t, "test\nhttps://www.niconico.jp/smXXXX?ref=rss_myvideo_rss2.0", messages[0].Content)
+		assert.Equal(t, "111", messages[0].ChannelID)
+	})
+
+	t.Run("最新のものがない場合投稿しないこと", func(t *testing.T) {
+		feed.Items = []*gofeed.Item{
+			{
+				Title:           "test",
+				Link:            "https://www.niconico.jp/smXXXX?ref=rss_myvideo_rss2.0",
+				PublishedParsed: &previousPostAt,
+			},
+		}
+		messages, err := run(ctx, discordSession, repo, webhook, feed)
+		assert.NoError(t, err)
+		assert.Len(t, messages, 0)
+	})
+}
